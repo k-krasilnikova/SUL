b6583d26e33be4a6e3d39b545afc16ec10ca5e58
k-krasilnikova <k.krasilnikova@itechart-group.com>
2021-12-08 17:47:46 +0300

    Added initial structure of the project

---
+++ backend/app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`SUL api started on port ${port}.`);
+});
