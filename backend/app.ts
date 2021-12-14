06c313457398cf5f221edef16d5187b836d97284
ICX\Aliaksandr.Shatsilau <drdr1282@mail.ru>
2021-12-14 18:20:40 +0300

    feat: add configs for eslint,prettier and jest

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { json } from 'body-parser';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
+
+app.use(json());
 
 app.listen(port, () => {
   console.log(`SUL api started on port ${port}.`);
