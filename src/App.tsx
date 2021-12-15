290063297b54fd4f36e12c0f1f5f2530d80caabb
yauheni.zhuk <yauheni.zhuk@aventri.com>
2021-12-16 00:12:48 +0300

    feature/V1-003 - configuration theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import {ThemeProvider} from "@mui/material";
+import theme from "./themeSettings";
 
 function App() {
-  return <></>;
+  return (
+      <ThemeProvider theme={theme}>
+         <div>
+          ABCD
+        </div>
+      </ThemeProvider>
+  );
 }
 
 export default App;
