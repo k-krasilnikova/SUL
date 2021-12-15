830671da22b8535f5b7faefc8d9ef05a33206f01
Julia <julia.maliutsina@gmail.com>
2021-12-15 21:01:55 +0300

    menu added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header"
+import Menu from "./components/Menu"
 
-function App() {
-  return <></>;
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <div className='menu'>
+        <Header />
+        <Menu /> 
+      </div> 
+    </BrowserRouter>  
+  );
 }
 
 export default App;
