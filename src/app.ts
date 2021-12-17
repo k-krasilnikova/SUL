c9bcebea841392b7c05f8367c8f8192db3b6cf66
ICXAliaksandr.Shatsilau <drdr1282@mail.ru>
2021-12-17 10:25:56 +0300

    refactor: correct logger name

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { json } from 'body-parser';
-import loggerMidleware from './utils/log/loggerMidleware';
+
+import loggerMiddleware from './utils/log/loggerMiddleware';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
 app.use(json());
-app.use(loggerMidleware);
+app.use(loggerMiddleware);
 app.listen(port, () => {
   console.log(`SUL api started on port ${port}.`);
 });
