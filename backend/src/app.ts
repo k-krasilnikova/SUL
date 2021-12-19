b047717f8ec547e62cc31f1fc2775c719bd09213
wooz1ewu <apolonik.danila2002@gmail.com>
2021-12-19 21:38:52 +0300

    feat: connection middleware implemented without corresponding error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { json } from 'body-parser';
 
+import connectionMiddleware from 'db/connection/connectionMiddleware';
+
 import loggerMiddleware from './utils/log/loggerMiddleware';
 
 dotenv.config();
@@ -9,7 +11,11 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(json());
+
 app.use(loggerMiddleware);
+
+app.use(connectionMiddleware);
+
 app.listen(port, () => {
   console.log(`SUL api started on port ${port}.`);
 });
