e057d031c0d6b1eb4a68dd238d6da895de4ed1f4
wooz1ewu <apolonik.danila2002@gmail.com>
2021-12-19 21:38:52 +0300

    feat: connection middleware implemented without corresponding error handling

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,8 @@
-export const ENVIROMENTS = {
+const ENVIROMENTS = {
   qa: 'test',
   prod: 'production',
   local: 'local',
 };
+const DEFAULT_CONNECTION_STRING = 'CONN_STRING_NOT_SET';
+
+export { DEFAULT_CONNECTION_STRING, ENVIROMENTS };
