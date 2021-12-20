541f2b6763eccc4704aefd77ca267eb7c30a8799
wooz1ewu <apolonik.danila2002@gmail.com>
2021-12-20 10:29:17 +0300

    configured initial users collection

--- backend/src/config/constants.ts
+++ backend/src/config/constants.ts
@@ -3,6 +3,15 @@ const ENVIROMENTS = {
   prod: 'production',
   local: 'local',
 };
+
 const DEFAULT_CONNECTION_STRING = 'CONN_STRING_NOT_SET';
 
-export { DEFAULT_CONNECTION_STRING, ENVIROMENTS };
+const USER_ROLES = {
+  ADMIN: 'admin',
+  MANAGER: 'manager',
+  EMPLOYEE: 'employee',
+};
+
+const SALT_ROUNDS = 10;
+
+export { DEFAULT_CONNECTION_STRING, USER_ROLES, SALT_ROUNDS, ENVIROMENTS };
