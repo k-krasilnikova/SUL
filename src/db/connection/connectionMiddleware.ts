f130d41ca50c73dfdc909f1e43f7b2b1f5ccdbc5
wooz1ewu <apolonik.danila2002@gmail.com>
2021-12-20 14:15:57 +0300

    feat: configured middlewarecall type and implemented error passing in connection middleware

--- src/db/connection/connectionMiddleware.ts
+++ src/db/connection/connectionMiddleware.ts
@@ -2,19 +2,17 @@ import { Request, Response } from 'express';
 import { connect } from 'mongoose';
 
 import { DEFAULT_CONNECTION_STRING } from 'config/constants';
+import { MiddlewareCall } from 'interfaces/middleware/common';
 
-const connectionMiddleware = async (req: Request, res: Response, next: () => void) => {
+const connectionMiddleware = async (req: Request, res: Response, next: MiddlewareCall) => {
   try {
     const CONNECTION_STRING: string = process.env.DATABASE_URL || DEFAULT_CONNECTION_STRING;
 
     await connect(CONNECTION_STRING);
 
     next();
-  } catch (err: any) {
-    // find a solution to define next()
-    // next(err);
-    // replace to logger after merging pr
-    console.log(err.message);
+  } catch (err: unknown) {
+    next(err);
   }
 };
 
