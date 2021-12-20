328db3ad5e231d2aae66e37ed9bf2e62d5ffa764
wooz1ewu <apolonik.danila2002@gmail.com>
2021-12-20 10:29:17 +0300

    configured initial users collection

---
+++ migrations/20211219190858-init_collections.js
@@ -0,0 +1,67 @@
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+
+module.exports = {
+  async up(db, client) {
+    DEFAULT_USERS_DOCS.map(async (doc) => {
+      const salt = bcrypt.genSaltSync(saltRounds);
+      doc.password = bcrypt.hashSync(doc.password, salt);
+      await db.collection('users').insertOne(doc);
+    });
+    // await db.collection('skills');
+    // await db.collection('courses');
+    // await db.collection('userSkills');
+    // await db.collection('userCourses');
+  },
+
+  async down(db, client) {
+    await db.collection('users').drop();
+    // await db.collection('skills').drop();
+    // await db.collection('courses').drop();
+    // await db.collection('userSkills').drop();
+    // await db.collection('userCourses').drop();
+  },
+};
+
+const DEFAULT_USERS_DOCS = [
+  {
+    name: 'admin',
+    password: 'admin',
+    role: 'admin',
+    fullName: 'Admin Admin',
+    position: 'SoftwareEngineer',
+    skills: [],
+    courses: [],
+    employees: [],
+    avatar: '',
+    birthday: '1970-01-01T00:00:00Z',
+    skype: 'admin',
+  },
+  {
+    name: 'user',
+    password: 'user',
+    role: 'employee',
+    fullName: 'User User',
+    position: 'SoftwareEngineer',
+    skills: [],
+    courses: [],
+    employees: [],
+    avatar: '',
+    birthday: '1970-01-01T00:00:00Z',
+    skype: 'user',
+  },
+  {
+    name: 'manager',
+    password: 'manager',
+    role: 'manager',
+    fullName: 'Manager Manager',
+    position: 'TeamManager',
+    skills: [],
+    courses: [],
+    employees: [],
+    avatar: '',
+    birthday: '1970-01-01T00:00:00Z',
+    skype: 'user',
+  },
+];
