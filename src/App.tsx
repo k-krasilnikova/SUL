85b6dc8773be25fc60953aab20897601a2aa3820
Irene Sinitsyna <hauteballe@gmail.com>
2021-12-22 01:32:31 +0300

    feat: setup react-query.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import {ThemeProvider} from "@mui/material";
+import { ThemeProvider } from '@mui/material';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-import theme from "./themeSettings";
+import { queryClient } from 'api/base';
 
-function App() {
-    return (
-        <ThemeProvider theme={theme}>
-            <div>
-                Some components
-            </div>
-        </ThemeProvider>
-    );
-}
+import theme from './themeSettings';
+
+const App: React.FC = () => (
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <div>Some components</div>
+    </ThemeProvider>
+    <ReactQueryDevtools />
+  </QueryClientProvider>
+);
 
 export default App;
