335610aaa0795d9dd031aaa7ac4c44780c5d2844
Irene Sinitsyna <hauteballe@gmail.com>
2021-12-22 01:32:31 +0300

    feat: setup react-query.

--- frontend/src/themeSettings.ts
+++ frontend/src/themeSettings.ts
@@ -1,30 +1,30 @@
-import {createTheme} from "@mui/material";
+import { createTheme } from '@mui/material';
 
 const colors = {
-    primaryColor: '#d83938',
-    secondaryColor: '#ffffff',
-    disabledTextColor: '#9b9b9b',
-    primaryTextColor: '#000000DE',
-    secondatyTextColor: '#ffffff'
-}
+  primaryColor: '#d83938',
+  secondaryColor: '#ffffff',
+  disabledTextColor: '#9b9b9b',
+  primaryTextColor: '#000000DE',
+  secondatyTextColor: '#ffffff',
+};
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: colors.primaryColor
-        },
-        secondary: {
-            main: colors.secondaryColor
-        },
-        text: {
-            primary: colors.primaryTextColor,
-            secondary: colors.secondatyTextColor,
-            disabled: colors.disabledTextColor
-        }
+  palette: {
+    primary: {
+      main: colors.primaryColor,
     },
-    typography: {
-        fontFamily: "'Lato', sans-serif"
-    }
-})
+    secondary: {
+      main: colors.secondaryColor,
+    },
+    text: {
+      primary: colors.primaryTextColor,
+      secondary: colors.secondatyTextColor,
+      disabled: colors.disabledTextColor,
+    },
+  },
+  typography: {
+    fontFamily: "'Lato', sans-serif",
+  },
+});
 
-export default theme
+export default theme;
