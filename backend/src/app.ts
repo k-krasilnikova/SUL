13da53a3736b2c81ee4b95ca209680ec74fdb8f9
ICX\Aliaksandr.Shatsilau <drdr1282@mail.ru>
2021-12-24 14:01:55 +0300

    fix: fix returned obj to plain js obj, refactor connection func

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,8 +4,9 @@ import { json } from 'body-parser';
 
 import { Routes } from 'enums/routesEnum';
 import routers from 'modules/routes/routes';
-import connectionMiddleware from 'db/connection/connectionMiddleware';
 import loggerMiddleware from 'modules/middlewares/loggerMiddleware';
+import { connect } from 'mongoose';
+import { DEFAULT_CONNECTION_STRING } from 'config/constants';
 
 dotenv.config();
 const app = express();
@@ -16,8 +17,12 @@ app.use(json());
 app.use(loggerMiddleware);
 app.use(`${Routes.namespace}`, routers);
 
-app.use(connectionMiddleware);
-
-app.listen(port, () => {
-  console.log(`SUL api started on port ${port}.`);
+app.listen(port, async () => {
+  try {
+    const CONNECTION_STRING: string = process.env.DATABASE_URL || DEFAULT_CONNECTION_STRING;
+    await connect(CONNECTION_STRING);
+    console.log(`SUL api started on port ${port}.`);
+  } catch (error) {
+    throw new Error('not connected');
+  }
 });
