134c4ac2a5f0657382410146b63a8a7ed61e295a
ICX\Aliaksandr.Shatsilau <drdr1282@mail.ru>
2021-12-24 14:14:59 +0300

    fix: refactor connection to db

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,9 +4,8 @@ import { json } from 'body-parser';
 
 import { Routes } from 'enums/routesEnum';
 import routers from 'modules/routes/routes';
+import connectionMiddleware from 'db/connection/connectionMiddleware';
 import loggerMiddleware from 'modules/middlewares/loggerMiddleware';
-import { connect } from 'mongoose';
-import { DEFAULT_CONNECTION_STRING } from 'config/constants';
 
 dotenv.config();
 const app = express();
@@ -14,15 +13,10 @@ const port = process.env.PORT;
 
 app.use(json());
 
+app.use(connectionMiddleware);
 app.use(loggerMiddleware);
 app.use(`${Routes.namespace}`, routers);
 
-app.listen(port, async () => {
-  try {
-    const CONNECTION_STRING: string = process.env.DATABASE_URL || DEFAULT_CONNECTION_STRING;
-    await connect(CONNECTION_STRING);
-    console.log(`SUL api started on port ${port}.`);
-  } catch (error) {
-    throw new Error('not connected');
-  }
+app.listen(port, () => {
+  console.log(`SUL api started on port ${port}.`);
 });
