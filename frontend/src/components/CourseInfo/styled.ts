3044b3b88d2525363db5ceabab47a03241990233
Julia <julia.maliutsina@gmail.com>
2021-12-29 19:09:08 +0300

    my courses page and components

--- frontend/src/components/CourseInfo/styled.ts
+++ frontend/src/components/CourseInfo/styled.ts
@@ -1,21 +1,38 @@
 import styled from 'styled-components';
 import { Typography, Box } from '@mui/material';
 
+import theme from 'themeSettings';
+
 export const InfoContainer = styled(Box)({
+  [theme.breakpoints.up('xs')]: {
+    minWidth: '326px',
+    margin: '10px',
+    height: '30px',
+    padding: '0px',
+  },
+  [theme.breakpoints.up('lg')]: {
+    minWidth: '410px',
+    margin: '20px 10px',
+    height: '50px',
+    padding: '10px 5px 10px 5px',
+  },
   display: 'inline-block',
-  width: '410px',
-  justifyContent: 'space-between',
-  margin: '20px',
   backgroundColor: 'white',
-  height: '50px',
-  padding: '10px 5px 10px 5px',
 });
 export const InfoItem = styled(Typography)({
+  [theme.breakpoints.up('xs')]: {
+    width: '80px',
+    padding: '5px',
+    fontSize: '10px',
+    lineHeight: '24px',
+  },
+  [theme.breakpoints.up('lg')]: {
+    width: '100px',
+    padding: '5px',
+    fontSize: '14px',
+    lineHeight: '24px',
+  },
   display: 'inline-flex',
-  width: '100px',
   verticalAlign: 'middle',
-  padding: '5px',
-  fontSize: '14px',
-  lineHeight: '24px',
   color: 'grey',
 });
--- frontend/src/pages/MyCourses/MyCourses.tsx
+++ frontend/src/pages/MyCourses/MyCourses.tsx
@@ -2,9 +2,40 @@ import React from 'react';
 
 import { AuthorizedLayout } from 'components/Layout';
 
+import CourseItem from './Course';
+import { PageContainer } from './styled';
+
+const INITIAL = {
+  title:
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s',
+  description:
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  language: 'English',
+  duration: '02:40:00',
+  lessons: '20',
+  link: 'link.io',
+};
+
 const MyCourses: React.FC = () => (
   <AuthorizedLayout pageName="My Courses">
-    <div>My Courses should be here</div>
+    <PageContainer>
+      <CourseItem
+        title={INITIAL.title}
+        description={INITIAL.description}
+        language={INITIAL.language}
+        duration={INITIAL.duration}
+        lessons={INITIAL.lessons}
+        link={INITIAL.link}
+      />
+      <CourseItem
+        title={INITIAL.title}
+        description={INITIAL.description}
+        language={INITIAL.language}
+        duration={INITIAL.duration}
+        lessons={INITIAL.lessons}
+        link={INITIAL.link}
+      />
+    </PageContainer>
   </AuthorizedLayout>
 );
 
