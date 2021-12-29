f9b067f179cede65319d9031237e0236271952cf
yauheni.zhuk <yauheni.zhuk@aventri.com>
2021-12-29 17:45:39 +0300

    feature/V1-026 - provide DB structure for client courses

---
+++ src/db/models/ClientCourses.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from 'mongoose';
+
+import { IClientCourses } from 'interfaces/entities/clientCourses';
+
+const clientCoursesSchema = new Schema<IClientCourses>({
+  _id: { type: Schema.Types.ObjectId },
+  title: { type: String, required: true },
+  description: { type: String },
+  technology: { type: String },
+  requiredSkills: [{ type: String }],
+  duration: { type: Number },
+  materials: [{ type: Schema.Types.ObjectId, ref: 'Materials' }],
+  testLink: { type: String },
+  status: { type: String },
+});
+
+const ClientCourseModel = model<IClientCourses>('ClientCourse', clientCoursesSchema);
+
+export default ClientCourseModel;
