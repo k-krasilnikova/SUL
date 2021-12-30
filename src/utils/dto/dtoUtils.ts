99e040c8d76222a86664582a9677f976904075c8
NikitaKhadnevich <vadim-swich@yandex.ru>
2021-12-30 12:25:09 +0300

    [feature/v1-007-Fetch] implement token,data fetch, also set,get cookie

--- src/utils/dto/dtoUtils.ts
+++ src/utils/dto/dtoUtils.ts
@@ -2,7 +2,7 @@ import { ITokens } from 'interfaces/auth/authInterfaces';
 import { IUser } from 'interfaces/entities/user';
 
 const generateInitialDto = (user: IUser, tokens?: ITokens) => {
-  const { _id: id, passwordHash, refreshToken, ...userDataToClient } = user;
+  const { passwordHash, refreshToken, ...userDataToClient } = user;
   return { ...tokens, ...userDataToClient };
 };
 
