b35b1d5a1e97074b90729a057bb473b29b942a54
k-krasilnikova <k.krasilnikova@itechart-group.com>
2022-01-04 13:29:07 +0300

    Added layout

---
+++ frontend/src/components/Layout/Header/Header.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
+
+import { PATHS } from 'constants/routes';
+import { User } from 'types/user';
+import {
+  LayoutHeader,
+  HeaderDivider,
+  SpaceHolder,
+  BrandLogo,
+  HeaderContent,
+  LogOut,
+} from 'components/Layout/styled';
+import UserDropDown from 'components/Layout/UserDropDown';
+import MyCoursesCounter from './MyCoursesCounter';
+const Header: React.FC<User> = ({
+  firstName,
+  lastName,
+  role,
+  unit,
+  department,
+  group,
+  myCoursesNumber,
+  avatar,
+}) => (
+  <LayoutHeader container>
+    <Grid item xs={3}>
+      <NavLink to={PATHS.home}>
+        <BrandLogo>Skill Up Level</BrandLogo>
+      </NavLink>
+    </Grid>
+    <Grid item xs={9}>
+      <HeaderContent>
+        <SpaceHolder />
+        <MyCoursesCounter myCoursesNumber={myCoursesNumber} />
+        <HeaderDivider />
+        <UserDropDown
+          firstName={firstName}
+          lastName={lastName}
+          role={role}
+          unit={unit}
+          department={department}
+          group={group}
+          avatar={avatar}
+        />
+        <HeaderDivider />
+        <LogOut />
+      </HeaderContent>
+    </Grid>
+  </LayoutHeader>
+);
+export default Header;
