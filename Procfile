68df621ca297c15f9c2566c897adba273ec85ab5
k-krasilnikova <k.krasilnikova@itechart-group.com>
2022-01-04 13:28:05 +0300

    Removed layout

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: 'cd ./frontend/ && npm run start'
-api: 'cd ./backend/ && npm run start'
--- frontend/src/components/layout/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as AuthorizedLayout } from './AuthorizedLayout';
-export { default as DefaultLayout } from './DefaultLayout';
