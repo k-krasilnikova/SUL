47ccc40dde13d266df00f1ffb329e4819a5698cb
NikitaKhadnevich <vadim-swich@yandex.ru>
2022-01-05 16:03:05 +0300

    [feature/v1-024] implement some styles, added basic respose design

--- frontend/src/components/Course/CourseInfo.tsx
+++ frontend/src/components/Course/CourseInfo.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import LanguageIcon from '@mui/icons-material/Language';
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import CollectionsBookmarkIcon from '@mui/icons-material/CollectionsBookmark';
 
 import { InfoContainer, InfoItem } from './styled';
 
 interface Props {
   duration: string;
   lessons: string;
-  language?: string | undefined;
-  link?: string | undefined;
 }
 
-const CourseInfo: React.FC<Props> = ({ duration, lessons, language, link }) => (
+const CourseInfo: React.FC<Props> = ({ duration, lessons }) => (
   <InfoContainer>
     <InfoItem>
       <AccessTimeIcon />
@@ -23,18 +19,6 @@ const CourseInfo: React.FC<Props> = ({ duration, lessons, language, link }) => (
       <PlayCircleOutlineIcon />
       {`${lessons} lessons`}
     </InfoItem>
-    {language && (
-      <InfoItem>
-        <LanguageIcon />
-        {language}
-      </InfoItem>
-    )}
-    {link && (
-      <InfoItem>
-        <CollectionsBookmarkIcon />
-        {link}
-      </InfoItem>
-    )}
   </InfoContainer>
 );
 
