59cae3c48d7d18ef2d15bc0814b33e8c2192ed7c
Aliaksandr <dfdf1534@gmail.com>
2022-01-05 10:59:20 +0300

    feat: add migration

--- backend/src/db/models/Course.ts
+++ backend/src/db/models/Course.ts
@@ -4,7 +4,6 @@ import { ICourse } from 'interfaces/entities/Icourses';
 import { materialSchema } from './Materials';
 
 const courseSchema = new Schema<ICourse>({
-  _id: { type: Schema.Types.ObjectId },
   title: { type: String, required: true, unique: true },
   description: { type: String },
   technology: [{ type: String, required: true }],
--- backend/src/db/models/Materials.ts
+++ backend/src/db/models/Materials.ts
@@ -3,11 +3,10 @@ import { Schema, model } from 'mongoose';
 import { IMaterial } from 'interfaces/entities/Imaterials';
 
 const materialSchema = new Schema<IMaterial>({
-  _id: { type: Schema.Types.ObjectId },
   content: [
     { stage: { type: Number }, content: [{ type: String }], isCompleted: { type: Boolean } },
   ],
-  technology: [{ types: String }],
+  technology: [{ type: String }],
 });
 
 const MaterialModel = model<IMaterial>('Materials', materialSchema);
