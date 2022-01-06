496f40c303bd076fe4d188444ad11d29166c4ab9
NikitaKhadnevich <vadim-swich@yandex.ru>
2022-01-06 14:06:52 +0300

    [feature/v1-024] Implement actual fix and components features

--- frontend/src/components/Image/styled.tsx
+++ frontend/src/components/Image/styled.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
-interface SizeProps {
-  width?: number;
-  height?: number;
+import { SIZE } from 'constants/sizes';
+
+interface Size {
+  size?: string;
 }
 
-export const ImageContainer = styled('div')<SizeProps>(({ width, height }) => ({
+export const ImageContainer = styled('div')<Size>(({ size }) => ({
   overflow: 'hidden',
   borderRadius: '10px',
   textAlign: 'center',
@@ -13,10 +14,16 @@ export const ImageContainer = styled('div')<SizeProps>(({ width, height }) => ({
   float: 'left',
   width: '150px',
   height: '100px',
-  ...(width && {
-    width: `${width}px`,
+  ...(size === SIZE.large && {
+    width: '450px',
+    height: '300px',
   }),
-  ...(height && {
-    height: `${height}px`,
+  ...(size === SIZE.medium && {
+    width: '300px',
+    height: '200px',
+  }),
+  ...(size === SIZE.small && {
+    width: '150px',
+    height: '100px',
   }),
 }));
