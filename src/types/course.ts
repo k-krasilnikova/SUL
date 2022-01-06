9961d58db08d9af088fd0cd9763e55f5a5d992df
Julia <julia.maliutsina@gmail.com>
2022-01-06 18:50:10 +0300

    interface Course to constants

---
+++ src/types/course.ts
@@ -0,0 +1,9 @@
+export interface Course {
+  title: string;
+  description: string;
+  technology: Array<string>;
+  requiredSkills: Array<string>;
+  duration: string;
+  testLink: string;
+  lessons: number;
+}
