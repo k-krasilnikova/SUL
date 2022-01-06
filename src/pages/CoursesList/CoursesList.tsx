2880156189d9eb890c49e2960b9c253bff5c7524
NikitaKhadnevich <vadim-swich@yandex.ru>
2022-01-06 16:28:08 +0300

    [feature/v1-025] Added custom hook and creted fetch courses func

--- src/pages/CoursesList/CoursesList.tsx
+++ src/pages/CoursesList/CoursesList.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import { useGetCourses } from 'api/courses';
+
 import { AuthorizedLayout } from 'components/Layout';
 
-const CoursesList: React.FC = () => (
-  <AuthorizedLayout pageName="Courses List">
-    <div>Courses List should be here</div>
-  </AuthorizedLayout>
-);
+const CoursesList: React.FC = () => {
+  const { data } = useGetCourses();
+  console.log(`data`, data);
+  return (
+    <AuthorizedLayout pageName="Courses List">
+      <div>Courses List should be here</div>
+    </AuthorizedLayout>
+  );
+};
 
 export default CoursesList;
