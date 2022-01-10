330a934f9aa6937d038068d430f8b5d2a5a72501
Julia <julia.maliutsina@gmail.com>
2022-01-10 20:44:32 +0300

    courses search implemented

--- src/pages/Profile/UserCourses/UserCourses.tsx
+++ src/pages/Profile/UserCourses/UserCourses.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import Divider from '@mui/material/Divider';
-import StarIcon from '@mui/icons-material/Star';
+import { useState } from 'react';
+import { Divider, Input } from '@mui/material';
+import { Star as StarIcon, Search as SearchIcon } from '@mui/icons-material';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 import { Course } from 'types/course';
 import isCourseCompleted from 'utils/helpers/isCourseCompleted';
@@ -19,30 +21,55 @@ interface CoursesProps {
   courses?: Array<Course>;
 }
 
-const UserCourses: React.FC<CoursesProps> = ({ courses }) => (
-  <CoursesBox>
-    <CoursesList>
-      {courses?.map((course) => (
-        <>
-          <Divider />
-          <CoursesListItem key={course._id}>
-            <CourseTitle>
-              <StarIcon
-                fontSize="small"
-                color={isCourseCompleted(course) ? 'primary' : 'disabled'}
-              />
-              <Title>{course.title}</Title>
-            </CourseTitle>
-            <MaterialsList>
-              {course.materials.map((material, id) => (
-                <CourseMaterialInfo key={id} material={material} />
-              ))}
-            </MaterialsList>
-          </CoursesListItem>
-        </>
-      ))}
-    </CoursesList>
-  </CoursesBox>
-);
+const UserCourses: React.FC<CoursesProps> = ({ courses }) => {
+  const [searchCourse, setSearchCourse] = useState('');
+  return (
+    <>
+      <CoursesBox>
+        <Input
+          disableUnderline={true}
+          placeholder="Search"
+          startAdornment={
+            <InputAdornment position="start">
+              <SearchIcon color="disabled" />
+            </InputAdornment>
+          }
+          onChange={(event) => {
+            setSearchCourse(event.target.value);
+          }}
+        />
+        <CoursesList>
+          {courses
+            ?.filter((course) => {
+              if (searchCourse === '') {
+                return course;
+              } else if (course.title.toLowerCase().includes(searchCourse.toLowerCase())) {
+                return course;
+              }
+            })
+            .map((course) => (
+              <>
+                <Divider />
+                <CoursesListItem key={course._id}>
+                  <CourseTitle>
+                    <StarIcon
+                      fontSize="small"
+                      color={isCourseCompleted(course) ? 'primary' : 'disabled'}
+                    />
+                    <Title>{course.title}</Title>
+                  </CourseTitle>
+                  <MaterialsList>
+                    {course.materials.map((material, id) => (
+                      <CourseMaterialInfo key={id} material={material} />
+                    ))}
+                  </MaterialsList>
+                </CoursesListItem>
+              </>
+            ))}
+        </CoursesList>
+      </CoursesBox>
+    </>
+  );
+};
 
 export default UserCourses;
