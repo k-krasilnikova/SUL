758dfde1ef8af463b810d8a16241dbd617e2f1ed
NikitaKhadnevich <vadim-swich@yandex.ru>
2022-01-11 18:18:39 +0300

    [feature/v1-007-FormFix] added style for helper text on form

---
+++ src/components/TextField/styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { Typography } from '@mui/material';
+
+export const WarningHelper = styled(Typography)({
+  lineHeight: '14px',
+  fontSize: '12px',
+  margin: '0px',
+});
+
+export default WarningHelper;
