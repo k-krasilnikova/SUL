355a6a7e65043f2b6c85ccd5c8fd07fa5d531cd9
NikitaKhadnevich <vadim-swich@yandex.ru>
2022-01-11 16:20:24 +0300

    [feature/v1-025] Added new type and rewrote useGetCourses Hook

--- src/types/course.ts
+++ src/types/course.ts
@@ -9,3 +9,17 @@ export interface Course {
   _id?: string;
   materials?: Array<string>;
 }
+export interface CourseResponse {
+  config: {
+    [key: string]: unknown | string | number;
+  };
+  data?: Array<Course>;
+  headers: {
+    [key: string]: string;
+  };
+  request: {
+    [key: string]: unknown | string | number;
+  };
+  status: number;
+  statusText: string;
+}
