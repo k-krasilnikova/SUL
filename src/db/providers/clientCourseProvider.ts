651488b493e2391cdd8f3ec9a4a5ae6d67053161
zhuk <evgeniy.zhuk.by@gmail.com>
2022-01-11 15:10:16 +0300

    feature/V1-029 - fix error on clients courses page

--- src/db/providers/clientCourseProvider.ts
+++ src/db/providers/clientCourseProvider.ts
@@ -2,7 +2,9 @@ import ClientCourseModel from '../models/ClientCourses';
 
 const getClientCoursesProvider = async (id: string) => {
   const courses = await ClientCourseModel.findById(id).lean();
-  if (!courses) throw new Error('User have not courses');
+  if (!courses) {
+    return [];
+  }
   return courses;
 };
 
