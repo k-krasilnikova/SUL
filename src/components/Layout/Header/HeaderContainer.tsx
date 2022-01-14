eff4b8cada223ceb28c211b111fef99c5a295419
Julia <julia.maliutsina@gmail.com>
2022-01-14 19:00:12 +0300

    moved vars to state

--- src/components/Layout/Header/HeaderContainer.tsx
+++ src/components/Layout/Header/HeaderContainer.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 import { User } from 'types/user';
 
@@ -8,17 +7,18 @@ import Header from './Header';
 const HeaderContainer: React.FC<User> = ({ firstName, lastName, avatar }) => {
   const [notificationsAnchor, setNotificationsAnchor] = useState<null | HTMLElement>(null);
   const [filterAnchor, setFilterAnchor] = useState<null | HTMLElement>(null);
+  const [isNotificationsOpen, setNotificationsOpen] = useState<boolean>(false);
+  const [isFilterOpen, setFilterOpen] = useState<boolean>(false);
 
   const handleNotificationsOpen = (event: React.MouseEvent<HTMLElement>) => {
     setNotificationsAnchor(notificationsAnchor ? null : event.currentTarget);
+    setNotificationsOpen(!isNotificationsOpen);
   };
 
   const handleFilterOpen = (event: React.MouseEvent<HTMLElement>) => {
     setFilterAnchor(filterAnchor ? null : event.currentTarget);
+    setFilterOpen(!isFilterOpen);
   };
-
-  const isNotificationsOpen = Boolean(notificationsAnchor);
-  const isFilterOpen = Boolean(filterAnchor);
 
   return (
     <Header
