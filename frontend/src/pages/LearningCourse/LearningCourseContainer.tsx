aa6d659ae750d10881535a1b594bddbeeb4472e6
Julia <julia.maliutsina@gmail.com>
2022-01-19 16:22:59 +0300

    fixed

--- frontend/src/pages/LearningCourse/LearningCourseContainer.tsx
+++ frontend/src/pages/LearningCourse/LearningCourseContainer.tsx
@@ -1,32 +1,28 @@
 import React, { useState } from 'react';
-/* import { useParams } from 'react-router'; */
 
 import { COURSE_TEXT, COURSE_DESCRIPTION } from 'constants/courseText';
 
 import LearningCourse from './LearningCourse';
 
+const maxStage = COURSE_TEXT.length;
+const minStage = 1;
+const stageChange = 1;
+
 const LearningCourseContainer: React.FC = () => {
-  /*
-  const params = useParams();
-  const { courseId } = params;
-  const { data } = useGetLearnCourse(courseId);
-  */
   const [stage, setStage] = useState(1);
   const [testEnabled, setTestEnabled] = useState(false);
 
-  const maxStage = COURSE_TEXT.length;
-
   const stageForward = () => {
     if (stage < maxStage) {
-      if (stage + 1 === maxStage && !testEnabled) {
+      if (stage + stageChange === maxStage && !testEnabled) {
         setTestEnabled(true);
       }
-      setStage(stage + 1);
+      setStage(stage + stageChange);
     }
   };
   const stageBack = () => {
-    if (stage > 1) {
-      setStage(stage - 1);
+    if (stage > minStage) {
+      setStage(stage - stageChange);
     }
   };
 
