83a8120e71078f671615c67b305b5462a130d443
Irene Sinitsyna <hauteballe@gmail.com>
2022-01-19 19:17:46 +0300

    fix: fixed PR conversations

--- frontend/src/pages/CoursesList/CoursesContainer.tsx
+++ frontend/src/pages/CoursesList/CoursesContainer.tsx
@@ -7,13 +7,12 @@ import useApplyCourse from 'api/courses/applyCourse';
 import CoursesList from './CoursesList';
 
 const CoursesContainer: React.FC = () => {
+  const params = useParams();
+  const { mutate } = useApplyCourse();
   const { data, isLoading } = useGetCourses();
 
-  const params = useParams();
-  const mutation = useApplyCourse();
-
   const handleApplyCourse = () => {
-    mutation.mutate(params._id);
+    mutate(params.courseId);
   };
 
   return <CoursesList data={data} isLoading={isLoading} handleApplyCourse={handleApplyCourse} />;
