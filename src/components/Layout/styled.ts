fd9cb01e4d88f80b996a2fd09d8f0b0928afe456
Julia <julia.maliutsina@gmail.com>
2022-01-20 16:12:46 +0300

    header shadow

---
+++ src/components/Layout/styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import Grid from '@mui/material/Grid';
+
+export const GridHeader = styled(Grid)({
+  boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+  zIndex: '10',
+});
