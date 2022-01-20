90a0d4dcafac014f36791d36383d644dde937181
NikitaKhadnevich <vadim-swich@yandex.ru>
2022-01-20 13:50:18 +0300

    [feature/v1-053] implemented left menu toggle

--- frontend/src/components/Layout/Menu/Menu.tsx
+++ frontend/src/components/Layout/Menu/Menu.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ListItemIcon, ListItemText, ListItemButton } from '@mui/material';
+import { ListItemIcon, ListItemText } from '@mui/material';
 
 import { MenuProps } from 'types/menu';
 
-import { MenuTabs } from './styled';
+import { MenuTabs, ListItemWraper } from './styled';
 
-const Menu: React.FC<MenuProps> = ({ menuList }) => (
-  <MenuTabs>
-    {menuList.map((item) => (
-      <ListItemButton key={item.title} component={Link} to={item.path}>
-        <ListItemIcon>{item.icon}</ListItemIcon>
-        <ListItemText primary={item.title} primaryTypographyProps={{ variant: 'h6' }} />
-      </ListItemButton>
-    ))}
-  </MenuTabs>
-);
+const Menu: React.FC<MenuProps> = ({ menuList, toggleTab }) => {
+  return (
+    <MenuTabs>
+      {menuList.map((item) => (
+        <ListItemWraper
+          key={item.title}
+          component={Link}
+          to={item.path}
+          selected={toggleTab === item.path}
+        >
+          <ListItemIcon>{item.icon}</ListItemIcon>
+          <ListItemText primary={item.title} primaryTypographyProps={{ variant: 'h6' }} />
+        </ListItemWraper>
+      ))}
+    </MenuTabs>
+  );
+};
 
 export default Menu;
