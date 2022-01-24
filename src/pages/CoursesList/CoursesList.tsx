486f49f7124e486d9a395b5720bf2665c0b27585
Julia <julia.maliutsina@gmail.com>
2022-01-24 14:47:39 +0300

    lazy loading of lists

--- src/pages/CoursesList/CoursesList.tsx
+++ src/pages/CoursesList/CoursesList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Link } from 'react-router-dom';
 import { Typography } from '@mui/material';
 
@@ -8,6 +8,7 @@ import { NoContent } from 'components/NoContent';
 import { NO_COURSES } from 'constants/messages';
 import { ResponseDataType } from 'types/responseData';
 import { PATHS } from 'constants/routes';
+import Loader from 'components/Loader';
 
 import { PageContainer, CourseButton, CourseActions, GridItem } from './styled';
 
@@ -19,24 +20,26 @@ const CoursesList: React.FC<ResponseDataType> = ({ data, isLoading, handleApplyC
       <PageContainer container>
         {data?.map((course) => (
           <GridItem key={course._id} item xl={6} lg={6} md={12} sm={12}>
-            <CourseItem
-              key={course._id}
-              title={course?.title}
-              description={course?.description}
-              duration={course?.duration}
-              lessons={course?.lessons}
-            >
-              <CourseActions>
-                <Link to={`${PATHS.coursesList}/${course._id}`}>
-                  <CourseButton color="primary" variant="contained">
-                    Details
+            <Suspense fallback={<Loader color="primary" />}>
+              <CourseItem
+                key={course._id}
+                title={course?.title}
+                description={course?.description}
+                duration={course?.duration}
+                lessons={course?.lessons}
+              >
+                <CourseActions>
+                  <Link to={`${PATHS.coursesList}/${course._id}`}>
+                    <CourseButton color="primary" variant="contained">
+                      Details
+                    </CourseButton>
+                  </Link>
+                  <CourseButton onClick={handleApplyCourse} color="primary" variant="contained">
+                    Start the course
                   </CourseButton>
-                </Link>
-                <CourseButton onClick={handleApplyCourse} color="primary" variant="contained">
-                  Start the course
-                </CourseButton>
-              </CourseActions>
-            </CourseItem>
+                </CourseActions>
+              </CourseItem>
+            </Suspense>
           </GridItem>
         ))}
       </PageContainer>
