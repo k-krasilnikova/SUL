c8d9297850d4e68fe97b6be49dfe3ffb465fad1d
ICXAliaksandr.Shatsilau <drdr1282@mail.ru>
2022-01-25 18:53:43 +0300

    feat: add get test endpoint

--- src/controllers/tests/getTest.ts
+++ src/controllers/tests/getTest.ts
@@ -1,3 +1,5 @@
+import { getClientCourseProvider } from 'db/providers/clientCourseProvider';
+import { getTestProvider } from 'db/providers/testProvider';
 import { Request, Response } from 'express';
 
 import { TMiddlewareCall } from 'interfaces/commonMiddleware';
@@ -6,6 +8,8 @@ import { isError } from 'utils/typeGuards/isError';
 const getTest = async (req: Request, res: Response, next: TMiddlewareCall) => {
   try {
     const { id: clientCourseId } = req.params;
+    const course = await getClientCourseProvider(clientCourseId);
+    console.log('in upper', course);
     const test = await getTestProvider(clientCourseId);
     res.json(test);
   } catch (err) {
