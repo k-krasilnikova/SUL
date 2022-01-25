8ac06d60b921d8d74965e1d5229114d83c032bb9
Aliaksandr <dfdf1534@gmail.com>
2022-01-26 02:26:49 +0300

    feat: add provider for get test

--- backend/src/db/providers/testProvider.ts
+++ backend/src/db/providers/testProvider.ts
@@ -4,30 +4,24 @@ import ClientCourseModel from 'db/models/ClientCourses';
 
 const getTestProvider = async (courseId: string) => {
   const test = await ClientCourseModel.aggregate([
+    { $match: { _id: new mongoose.Types.ObjectId(courseId) } },
     {
-      $match: { _id: new mongoose.Types.ObjectId(courseId) },
       $lookup: {
         from: 'courses',
         localField: 'course',
         foreignField: '_id',
-        as: 'commonCourse',
+        as: 'nested_course',
       },
     },
-    { $unwind: '$commonCourse' },
     {
       $lookup: {
-        from: 'test',
-        localField: 'commonCourse.test',
+        from: 'tests',
+        localField: 'nested_course.test',
         foreignField: '_id',
-        as: 'userTest',
+        as: 'myTest',
       },
     },
-    { $unwind: '$userTest' },
-    {
-      $project: {
-        userTest: 1,
-      },
-    },
+    { $project: { myTest: 1, _id: 0 } },
   ]);
   return test;
 };
