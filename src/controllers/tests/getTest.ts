54d523da6c0e20efa30e3a90263cfce93eefaca6
ICXAliaksandr.Shatsilau <drdr1282@mail.ru>
2022-01-26 16:56:38 +0300

    feat: add error handler

--- src/controllers/tests/getTest.ts
+++ src/controllers/tests/getTest.ts
@@ -8,6 +8,9 @@ const getTest = async (req: Request, res: Response, next: TMiddlewareCall) => {
   try {
     const { id: clientCourseId } = req.params;
     const test = await getTestProvider(clientCourseId);
+    if (!test.length) {
+      throw new Error('test not found');
+    }
     res.json(test);
   } catch (err) {
     if (isError(err)) {
