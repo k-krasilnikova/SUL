09938c2af13abcc9f80a6eb66c846b0023ecf0ec
Julia <julia.maliutsina@gmail.com>
2022-01-27 14:58:48 +0300

    closing filters & notifications

--- frontend/src/components/Layout/Header/HeaderContainer.tsx
+++ frontend/src/components/Layout/Header/HeaderContainer.tsx
@@ -6,29 +6,20 @@ import { useLogOut } from 'api/logOut';
 import Header from './Header';
 
 const HeaderContainer: React.FC<User> = ({ firstName, lastName, avatar }) => {
-  const [notificationsAnchor, setNotificationsAnchor] = useState<null | HTMLElement>(null);
-  const [filterAnchor, setFilterAnchor] = useState<null | HTMLElement>(null);
   const [isNotificationsOpen, setNotificationsOpen] = useState<boolean>(false);
   const [isFilterOpen, setFilterOpen] = useState<boolean>(false);
 
-  const handleNotificationsOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setNotificationsAnchor(notificationsAnchor ? null : event.currentTarget);
+  const handleNotificationsOpen = () => {
     setNotificationsOpen(!isNotificationsOpen);
   };
-
-  const handleFilterOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setFilterAnchor(filterAnchor ? null : event.currentTarget);
+  const handleFilterOpen = () => {
     setFilterOpen(!isFilterOpen);
   };
-
   const handleNotificationsClose = () => {
-    setNotificationsOpen(!isNotificationsOpen);
-    setNotificationsAnchor(null);
+    setNotificationsOpen(false);
   };
-
   const handleFilterClose = () => {
-    setFilterOpen(!isFilterOpen);
-    setFilterAnchor(null);
+    setFilterOpen(false);
   };
 
   const { mutateAsync } = useLogOut();
@@ -40,20 +31,20 @@ const HeaderContainer: React.FC<User> = ({ firstName, lastName, avatar }) => {
   };
 
   return (
-    <Header
-      firstName={firstName}
-      lastName={lastName}
-      avatar={avatar}
-      isNotificationsOpen={isNotificationsOpen}
-      isFilterOpen={isFilterOpen}
-      notificationsAnchor={notificationsAnchor}
-      filterAnchor={filterAnchor}
-      handleNotificationsOpen={handleNotificationsOpen}
-      handleFilterOpen={handleFilterOpen}
-      handleNotificationsClose={handleNotificationsClose}
-      handleFilterClose={handleFilterClose}
-      handleLogOut={handleLogOut}
-    />
+    <>
+      <Header
+        firstName={firstName}
+        lastName={lastName}
+        avatar={avatar}
+        isNotificationsOpen={isNotificationsOpen}
+        isFilterOpen={isFilterOpen}
+        handleNotificationsOpen={handleNotificationsOpen}
+        handleFilterOpen={handleFilterOpen}
+        handleNotificationsClose={handleNotificationsClose}
+        handleFilterClose={handleFilterClose}
+        handleLogOut={handleLogOut}
+      />
+    </>
   );
 };
 export default HeaderContainer;
