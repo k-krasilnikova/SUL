2f241dfadb46d2a56795769b5bf00b3109ce4685
yauheni.zhuk <yauheni.zhuk@aventri.com>
2022-01-28 13:42:11 +0300

    fix/V1-048 - refactored logout function

--- src/routing/accountRouter.ts
+++ src/routing/accountRouter.ts
@@ -4,11 +4,17 @@ import { SubRoutes } from 'enums/routesEnum';
 import login from 'controllers/auth/login';
 import refresh from 'controllers/auth/refresh';
 import logout from 'controllers/auth/logout';
+import withAuth from '../middlewares/authMiddleware';
+import { USER_ROLES } from '../config/constants';
 
 const accountRouter = Router();
 
 accountRouter.post(`${SubRoutes.login}`, login);
-accountRouter.get(`${SubRoutes.logout}`, logout);
+accountRouter.get(
+  `${SubRoutes.logout}`,
+  withAuth([USER_ROLES.EMPLOYEE, USER_ROLES.ADMIN, USER_ROLES.MANAGER]),
+  logout,
+);
 accountRouter.get(`${SubRoutes.refresh}`, refresh);
 
 export default accountRouter;
