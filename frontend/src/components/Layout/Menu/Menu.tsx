0ae5a44a638f5899e8ce809020771208801fb1ca
NikitaKhadnevich <vadim-swich@yandex.ru>
2022-02-02 13:09:24 +0300

    [bugfix-feature-v1/053] minor fix ib head of components

--- frontend/src/components/Layout/Menu/Menu.tsx
+++ frontend/src/components/Layout/Menu/Menu.tsx
@@ -6,49 +6,47 @@ import { IMenuProps } from 'types/menu';
 
 import { MenuTabs, MenuTabsWrapper, TabWrapper, ItemText, LeftArrow, RightArrow } from './styled';
 
-const Menu: React.FC<IMenuProps> = ({ menuList, classes, pathname, isSqueeze, handleSqueeze }) => {
-  return (
-    <MenuTabs>
-      {isSqueeze ? <RightArrow onClick={handleSqueeze} /> : <LeftArrow onClick={handleSqueeze} />}
-      <MenuTabsWrapper>
-        {menuList.map((item) =>
-          pathname?.includes(item.path) ? (
-            <TabWrapper
-              id={item.path}
-              key={item.title}
-              component={Link}
-              classes={{ root: classes.selected }}
-              to={item.path}
-            >
-              <ListItemIcon classes={{ root: classes.selectedLogo }}>{item.icon}</ListItemIcon>
-              <ItemText
-                isSqueeze={isSqueeze}
-                primary={item.title}
-                primaryTypographyProps={{ variant: 'h6' }}
-                classes={{ root: classes.selectedText }}
-              />
-            </TabWrapper>
-          ) : (
-            <TabWrapper
-              id={item.path}
-              key={item.title}
-              component={Link}
+const Menu: React.FC<IMenuProps> = ({ menuList, classes, pathname, isSqueeze, handleSqueeze }) => (
+  <MenuTabs>
+    {isSqueeze ? <RightArrow onClick={handleSqueeze} /> : <LeftArrow onClick={handleSqueeze} />}
+    <MenuTabsWrapper>
+      {menuList.map((item) =>
+        pathname?.includes(item.path) ? (
+          <TabWrapper
+            id={item.path}
+            key={item.title}
+            component={Link}
+            classes={{ root: classes.selected }}
+            to={item.path}
+          >
+            <ListItemIcon classes={{ root: classes.selectedLogo }}>{item.icon}</ListItemIcon>
+            <ItemText
+              isSqueeze={isSqueeze}
+              primary={item.title}
+              primaryTypographyProps={{ variant: 'h6' }}
+              classes={{ root: classes.selectedText }}
+            />
+          </TabWrapper>
+        ) : (
+          <TabWrapper
+            id={item.path}
+            key={item.title}
+            component={Link}
+            classes={{ root: classes.default }}
+            to={item.path}
+          >
+            <ListItemIcon classes={{ root: classes.default }}>{item.icon}</ListItemIcon>
+            <ItemText
+              isSqueeze={isSqueeze}
+              primary={item.title}
+              primaryTypographyProps={{ variant: 'h6' }}
               classes={{ root: classes.default }}
-              to={item.path}
-            >
-              <ListItemIcon classes={{ root: classes.default }}>{item.icon}</ListItemIcon>
-              <ItemText
-                isSqueeze={isSqueeze}
-                primary={item.title}
-                primaryTypographyProps={{ variant: 'h6' }}
-                classes={{ root: classes.default }}
-              />
-            </TabWrapper>
-          ),
-        )}
-      </MenuTabsWrapper>
-    </MenuTabs>
-  );
-};
+            />
+          </TabWrapper>
+        ),
+      )}
+    </MenuTabsWrapper>
+  </MenuTabs>
+);
 
 export default Menu;
