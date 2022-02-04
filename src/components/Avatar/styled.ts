a18aebbf25d7bd8ed362af92c38ab6b6b86c0f9f
Julia <julia.maliutsina@gmail.com>
2022-02-04 16:16:01 +0300

    Merge branch 'develop' into bugfix/v1-065

--- src/components/Avatar/styled.ts
+++ src/components/Avatar/styled.ts
@@ -5,38 +5,44 @@ import { SIZE } from 'constants/sizes';
 import theme from 'themeSettings';
 
 interface Size {
-  avatarSize?: string;
+  size?: string;
 }
 
-export const UserAvatarRounded = styled(Avatar)<Size>(({ avatarSize }) => ({
+export const UserAvatarRounded = styled(Avatar)<Size>(({ size }) => ({
   display: 'inline-block',
   margin: '1% auto',
   width: '100px',
   height: '100px',
   [theme.breakpoints.up('xs')]: {
-    ...(avatarSize === SIZE.large && {
-      width: '120px',
-      height: '120px',
-    }),
-    ...(avatarSize === SIZE.medium && {
+    ...(size === SIZE.large && {
       width: '80px',
       height: '80px',
     }),
-    ...(avatarSize === SIZE.small && {
+    ...(size === SIZE.medium && {
+      width: '60px',
+      height: '60px',
+    }),
+    ...(size === SIZE.small && {
       width: '40px',
       height: '40px',
     }),
   },
   [theme.breakpoints.up('lg')]: {
-    ...(avatarSize === SIZE.large && {
+    ...(size === SIZE.large && {
+      width: '132px',
+      height: '132px',
+    }),
+  },
+  [theme.breakpoints.up('xl')]: {
+    ...(size === SIZE.large && {
       width: '220px',
       height: '220px',
     }),
-    ...(avatarSize === SIZE.medium && {
+    ...(size === SIZE.medium && {
       width: '100px',
       height: '100px',
     }),
-    ...(avatarSize === SIZE.small && {
+    ...(size === SIZE.small && {
       width: '50px',
       height: '50px',
     }),
