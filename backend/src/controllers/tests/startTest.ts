6c3dde8acb9187316c56a42fb65437b84107453d
yauheni.zhuk <yauheni.zhuk@aventri.com>
2022-02-04 17:14:03 +0300

    fix/V1-058 - a few changes

--- backend/src/controllers/tests/startTest.ts
+++ backend/src/controllers/tests/startTest.ts
@@ -4,14 +4,13 @@ import { TMiddlewareCall } from 'interfaces/commonMiddleware';
 import { isError } from 'utils/typeGuards/isError';
 import { getTestProvider } from 'db/providers/testProvider';
 import { updateCourseStatus } from 'db/providers/clientCourseProvider';
-
-const IN_TESTING = 'inTesting';
+import CourseStatus from 'enums/coursesEnums';
 
 const startTest = async (req: Request, res: Response, next: TMiddlewareCall) => {
   try {
     const { id: clientCourseId } = req.params;
     const test = await getTestProvider(clientCourseId);
-    await updateCourseStatus(clientCourseId, IN_TESTING);
+    await updateCourseStatus(clientCourseId, CourseStatus.testing);
     res.json(test);
   } catch (err) {
     if (isError(err)) {
