fc669713a61260e84460fdf253939d8d8966bf3e
yauheni.zhuk <yauheni.zhuk@aventri.com>
2022-02-04 17:16:06 +0300

    fix/V1-058 - fix imports

--- backend/src/routing/testRouter.ts
+++ backend/src/routing/testRouter.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { Params } from '../enums/routesEnum';
-import withAuth from '../middlewares/authMiddleware';
-import { USER_ROLES } from '../config/constants';
-import startTest from '../controllers/tests/startTest';
+
+import { Params } from 'enums/routesEnum';
+import withAuth from 'middlewares/authMiddleware';
+import { USER_ROLES } from 'config/constants';
+import startTest from 'controllers/tests/startTest';
 
 const testRouter = Router();
 testRouter.get(
