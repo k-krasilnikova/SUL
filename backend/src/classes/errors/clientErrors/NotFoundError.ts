a821da64277d71bb659b3abd694933311250fbb5
Daniel <apolonik.danila2002@gmail.com>
2022-02-04 15:17:48 +0300

    refactor/feat: server errors classes implemented, client errors classes refactored

--- backend/src/classes/errors/clientErrors/NotFoundError.ts
+++ backend/src/classes/errors/clientErrors/NotFoundError.ts
@@ -5,7 +5,7 @@ import CommonHttpError from '../common/CommonHttpError';
 class NotFoundError extends CommonHttpError {
   constructor(message: string) {
     super(message);
-    this.statusCode = STATUS_CODES.clientErros.BAD_REQUEST;
+    this.statusCode = STATUS_CODES.clientErrors.NOT_FOUND;
   }
 }
 
