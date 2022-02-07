6560ec6f1a0533b0da5933b08da2edbf3581a782
Daniel <apolonik.danila2002@gmail.com>
2022-02-07 10:16:00 +0300

    feat/refacor: added internal error handler, refactored internal error class(constructor with no args)

--- backend/src/classes/errors/serverErrors/InternalServerError.ts
+++ backend/src/classes/errors/serverErrors/InternalServerError.ts
@@ -3,8 +3,8 @@ import { STATUS_CODES } from 'config/constants';
 import CommonHttpError from '../common/CommonHttpError';
 
 class InternalServerError extends CommonHttpError {
-  constructor(message: string) {
-    super(message);
+  constructor() {
+    super('Unknown server error.');
     this.statusCode = STATUS_CODES.serverErrors.INTERNAL_SERVER_ERROR;
   }
 }
