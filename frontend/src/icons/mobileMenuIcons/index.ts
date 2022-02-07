44501c5a5edf5ffcd85996b8cb1c4e6ee71a88d3
Julia <julia.maliutsina@gmail.com>
2022-02-07 17:30:20 +0300

    mobile menu

---
+++ frontend/src/icons/mobileMenuIcons/index.ts
@@ -0,0 +1,6 @@
+export { ReactComponent as coursesListIcon } from './coursesList.svg';
+export { ReactComponent as myCoursesIcon } from './myCourses.svg';
+export { ReactComponent as helpIcon } from './help.svg';
+export { ReactComponent as pendingIcon } from './pending.svg';
+export { ReactComponent as employeesIcon } from './employees.svg';
+export { ReactComponent as skillsIcon } from './skills.svg';
