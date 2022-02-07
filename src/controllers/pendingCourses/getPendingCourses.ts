9b5fde2aa9438c266c26a96daa7b3999d57d396e
yauheni.zhuk <yauheni.zhuk@aventri.com>
2022-02-07 13:49:23 +0300

    fix/V1-079 - fix eslint error

--- src/controllers/pendingCourses/getPendingCourses.ts
+++ src/controllers/pendingCourses/getPendingCourses.ts
@@ -8,8 +8,10 @@ import { getPendingCoursesProvider } from 'db/providers/pendingCoursesProvider';
 const getPendingCourses = async (req: Request, res: Response, next: TMiddlewareCall) => {
   const { id: userId } = res.locals;
   try {
-    const user: IUser = await getPendingCoursesProvider(userId);
-    res.json(user.pendingCourses);
+    if (typeof userId === 'string') {
+      const user: IUser = await getPendingCoursesProvider(userId);
+      res.json(user.pendingCourses);
+    }
   } catch (error) {
     if (isError(error)) {
       next(error);
