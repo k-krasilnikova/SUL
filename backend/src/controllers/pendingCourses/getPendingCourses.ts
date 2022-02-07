87b46168c51a24ad07469693babacac9d4ddd242
yauheni.zhuk <yauheni.zhuk@aventri.com>
2022-02-07 23:26:01 +0300

    fix/V1-079 - refactored

--- backend/src/controllers/pendingCourses/getPendingCourses.ts
+++ backend/src/controllers/pendingCourses/getPendingCourses.ts
@@ -1,17 +1,20 @@
 import { Request, Response } from 'express';
+import { ObjectId } from 'mongoose';
 
 import { isError } from 'utils/typeGuards/isError';
 import { TMiddlewareCall } from 'interfaces/commonMiddleware';
-import { IUser } from 'interfaces/Ientities/Iusers';
+import { IPendingCourses, IUser } from 'interfaces/Ientities/Iusers';
 import { getPendingCoursesProvider } from 'db/providers/pendingCoursesProvider';
 
-const getPendingCourses = async (req: Request, res: Response, next: TMiddlewareCall) => {
+const getPendingCourses = async (
+  req: Request,
+  res: Response<IPendingCourses | ObjectId[], { id: string }>,
+  next: TMiddlewareCall,
+) => {
   const { id: userId } = res.locals;
   try {
-    if (typeof userId === 'string') {
-      const user: IUser = await getPendingCoursesProvider(userId);
-      res.json(user.pendingCourses);
-    }
+    const user: IUser = await getPendingCoursesProvider(userId);
+    res.json(user.pendingCourses);
   } catch (error) {
     if (isError(error)) {
       next(error);
