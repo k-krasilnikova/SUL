e7d70435c7398e572e322ffbce16134c3162bdbb
NikitaKhadnevich <vadim-swich@yandex.ru>
2022-02-09 18:27:12 +0300

    [feature/v1-066] Added svg spinner

--- src/animations/index.ts
+++ src/animations/index.ts
@@ -1,1 +1,2 @@
-export { default as buttonSpinner } from './buttonSpinner.gif';
+// export { default as buttonSpinner } from './buttonSpinner.gif';
+export { default as buttonSpinner } from './buttonLoader.svg';
