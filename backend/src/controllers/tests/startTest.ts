f5d00e30160e06dcad73f2afac9f0e9b603b7b9c
Aliaksandr <dfdf1534@gmail.com>
2022-02-10 15:25:14 +0300

    refactor: change error boundary in pass test

--- backend/src/controllers/tests/startTest.ts
+++ backend/src/controllers/tests/startTest.ts
@@ -3,12 +3,19 @@ import { Request, Response } from 'express';
 import { TMiddlewareCall } from 'interfaces/commonMiddleware';
 import { isError } from 'utils/typeGuards/isError';
 import { getTestProvider } from 'db/providers/testProvider';
-import { updateCourseStatus } from 'db/providers/clientCourseProvider';
+import { getCurrentProgress, updateCourseStatus } from 'db/providers/clientCourseProvider';
 import CourseStatus from 'enums/coursesEnums';
+import BadRequestError from 'classes/errors/clientErrors/BadRequestError';
+import { INITIAL_INDX, REQUIRED_PCT } from 'config/constants';
 
 const startTest = async (req: Request, res: Response, next: TMiddlewareCall) => {
   try {
     const { id: clientCourseId } = req.params;
+
+    const progress = await getCurrentProgress(clientCourseId);
+    if (progress[INITIAL_INDX].currProgress < REQUIRED_PCT) {
+      throw new BadRequestError("Can not start test till all the stages haven't been passed.");
+    }
     const test = await getTestProvider(clientCourseId);
     await updateCourseStatus(clientCourseId, CourseStatus.testing);
     res.json(test);
