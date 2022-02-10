222fabc0ae0e9fbdc40dfe82947273ce1c2d1c55
NikitaKhadnevich <vadim-swich@yandex.ru>
2022-02-10 10:15:36 +0300

    [feature/v1-066] Added new wrapper for correctly work

--- src/components/ButtonLoader/styled.ts
+++ src/components/ButtonLoader/styled.ts
@@ -1,6 +1,19 @@
 import styled from 'styled-components';
+import { Icon } from '@mui/material';
+import { ReactFragment } from 'react';
+
+interface Props {
+  buttonSpinner?: ReactFragment;
+  fontSize?: string;
+  component?: ReactFragment;
+}
 
 export const LoaderIcon = styled('div')({
   width: '40px !important',
   height: '40px !important',
+});
+
+export const CurrentIcon = styled(Icon)<Props>({
+  width: '100% !important',
+  height: '100% !important',
 });
