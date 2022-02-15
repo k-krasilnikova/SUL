92abd6fb8307e7821f8002238f0e4be8262d25d8
Irene Sinitsyna <hauteballe@gmail.com>
2022-02-16 00:41:32 +0300

    feat: implement responsive Help page

--- frontend/src/components/Arrows/arrowForward.tsx
+++ frontend/src/components/Arrows/arrowForward.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
+import { Arrow } from './styled';
+
 interface Props {
   arrowDisabled?: boolean;
 }
 
 const arrowForward: React.FC<Props> = ({ arrowDisabled }) => (
-  <svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <Arrow width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M1 17L9 9L1 0.999999" stroke={arrowDisabled ? '#b6b6bf' : '#131313'} />
-  </svg>
+  </Arrow>
 );
 
 export default arrowForward;
