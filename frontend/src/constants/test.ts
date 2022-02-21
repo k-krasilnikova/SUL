bf0910ef41fab4f8d9799ed5e0382567af24a179
Irene Sinitsyna <hauteballe@gmail.com>
2022-02-21 11:57:59 +0300

    refactor

--- frontend/src/constants/test.ts
+++ frontend/src/constants/test.ts
@@ -1,6 +1,7 @@
 export const MAX_STAGE_INITIAL = 1;
 export const MIN_STAGE = 1;
 export const STAGE_CHANGE = 1;
+export const PERCENTAGE = 100;
 
 export const TEST_STATUS = {
   notPassed: 'not passed',
