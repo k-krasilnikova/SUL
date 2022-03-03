cf564ba23cbf50a20f579d4eaf0521d1932f5f57
Irene Sinitsyna <hauteballe@gmail.com>
2022-03-03 12:41:43 +0300

    init: started responsive Test&Result design task

--- frontend/src/pages/PassingTest/QuestionItem/styled.ts
+++ frontend/src/pages/PassingTest/QuestionItem/styled.ts
@@ -20,27 +20,61 @@ export const StyledFormControlLabel = styled(FormControlLabel)({
     marginLeft: '28px',
     fontFamily: theme.typography.fontFamily,
     color: theme.palette.text.primary,
+    [theme.breakpoints.down('xl')]: {
+      fontSize: '18px !important',
+      marginLeft: '24px',
+      maxWidth: '447px',
+    },
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '14px !important',
+      maxWidth: '258px',
+      marginLeft: '8px',
+    },
   },
 });
 
 export const QuestionAndStageBox = styled(Box)({
   display: 'flex',
   justifyContent: 'space-between',
+  [theme.breakpoints.down('xl')]: {
+    alignContent: 'flex-start',
+  },
 });
 
 export const TestQuestion = styled(Typography)({
   fontWeight: '500 !important',
   fontSize: '24px !important',
   color: '#000000',
+  [theme.breakpoints.down('xl')]: {
+    fontSize: '22px !important',
+  },
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '16px !important',
+    color: '#131313',
+  },
 });
 
 export const StageWrapper = styled(Box)({
   marginRight: '332px',
+  [theme.breakpoints.down('xl')]: {
+    marginRight: '190px !important',
+    marginLeft: '146px !important',
+  },
+  [theme.breakpoints.down('md')]: {
+    marginRight: '15vw !important',
+  },
+  [theme.breakpoints.down('sm')]: {
+    marginRight: '11px !important',
+    marginLeft: '27px !important',
+  },
 });
 
 export const Stage = styled(Typography)({
   fontSize: '18px !important',
   color: '#131313',
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '14px !important',
+  },
 });
 
 export const AnswersBox = styled(Box)({
