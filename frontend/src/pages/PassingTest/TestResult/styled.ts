f1bca0ed0bbefaddd675f5934327daaf09cd0efb
Irene Sinitsyna <hauteballe@gmail.com>
2022-03-03 13:54:18 +0300

    feat: implemented Test result(failed) styles for mobile

--- frontend/src/pages/PassingTest/TestResult/styled.ts
+++ frontend/src/pages/PassingTest/TestResult/styled.ts
@@ -3,39 +3,64 @@ import { Box } from '@mui/system';
 import { Divider, Typography } from '@mui/material';
 
 import Button from 'components/Button/Button';
+import theme from 'themeSettings';
 
 export const TestResultBox = styled(Box)({
   margin: '64px 332px 199px 120px',
   display: 'flex',
   flexDirection: 'column',
+  [theme.breakpoints.down('sm')]: {
+    margin: '32px 42px 0 16px',
+  },
+});
+
+export const TitleBox = styled(Box)({
+  [theme.breakpoints.down('sm')]: {
+    marginLeft: '16px',
+  },
 });
 
 export const TestResultTitle = styled(Typography)({
   fontWeight: '500 !important',
   fontSize: '32px !important',
   color: '#131313',
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '20px !important',
+  },
 });
 
 export const ProgressBarBox = styled(Box)({
   marginBottom: '56px',
   display: 'flex',
   justifyContent: 'flex-start',
+  [theme.breakpoints.down('sm')]: {
+    marginBottom: '24px !important',
+  },
 });
 
 export const TestResultText = styled(Typography)({
   fontWeight: '500 !important',
   fontSize: '32px !important',
   color: '#131313',
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '18px !important',
+  },
 });
 
 export const SkillsText = styled(Typography)({
   fontWeight: '500 !important',
   fontSize: '32px !important',
   color: '#131313',
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '16px !important',
+  },
 });
 
 export const TestSkillsBox = styled(Box)({
   marginTop: '20px',
+  [theme.breakpoints.down('sm')]: {
+    marginTop: '16px !important',
+  },
 });
 
 export const ButtonBox = styled(Box)({
@@ -47,27 +72,49 @@ export const SubmitButton = styled(Button)({
   height: '50px',
   width: '103px',
   fontSize: '18px !important',
+  [theme.breakpoints.down('sm')]: {
+    height: '26px',
+    width: '56px',
+    minWidth: '44px !important',
+    fontSize: '14px !important',
+    fontWeight: '500 !important',
+  },
 });
 
 export const ContentBox = styled(Box)({
   display: 'flex',
   marginTop: '48px',
   marginBottom: '220px',
+  [theme.breakpoints.down('sm')]: {
+    marginTop: '32px !important',
+    marginBottom: '32px !important',
+    flexDirection: 'column',
+  },
 });
 
 export const ResultBox = styled(Box)({
   marginLeft: '148px',
+  [theme.breakpoints.down('sm')]: {
+    marginLeft: '0 !important',
+  },
 });
 
 export const AboutSkillsBox = styled(Box)({
   marginTop: '44px !important',
   marginBottom: '32px !important',
+  [theme.breakpoints.down('sm')]: {
+    marginTop: '24px !important',
+    marginBottom: '0 !important',
+  },
 });
 
 export const FailedCourseText = styled(Typography)({
   fontSize: '32px !important',
   fontWeight: '400 !important',
   color: '#131313',
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '16px !important',
+  },
 });
 
 export const StyledDivider = styled(Divider)({
