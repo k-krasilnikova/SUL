1abb8cb84cbd4c2ed0ae099864f42ca04819854b
Aliaksandr <dfdf1534@gmail.com>
2022-03-04 15:31:33 +0300

    feat: add mail notifications

--- src/db/providers/userProvider.ts
+++ src/db/providers/userProvider.ts
@@ -5,7 +5,7 @@ import BadRequestError from 'classes/errors/clientErrors/BadRequestError';
 import NotFoundError from 'classes/errors/clientErrors/NotFoundError';
 import UserModel from 'db/models/User';
 
-const getUserProvider = async (userId: string) => {
+const getUserProvider = async (userId: string | ObjectId) => {
   const dbUser = await UserModel.findById(userId).lean();
   if (!dbUser) {
     throw new NotFoundError('User not found.');
