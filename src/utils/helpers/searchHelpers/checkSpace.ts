1511ec349563b3aa0e65e59fc53095d9d02e45e1
Julia <julia.maliutsina@gmail.com>
2022-03-12 16:01:30 +0300

    fixed

--- src/utils/helpers/searchHelpers/checkSpace.ts
+++ src/utils/helpers/searchHelpers/checkSpace.ts
@@ -1,7 +1,7 @@
 const checkWhitespace = (event: React.KeyboardEvent, inputValue: string): void => {
   const { key } = event;
   const formattedKey = key.trim();
-  if (!formattedKey && inputValue.length) {
+  if (!formattedKey && !inputValue.length) {
     event.preventDefault();
   }
 };
