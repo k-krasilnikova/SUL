d4120b8696dc31538dd36cb57d3a4e69400c72ae
Danila.Apalonik <Danila.Apalonik@itechart-group.com>
2022-03-16 13:42:13 +0300

    fix: mapping to one line expression fixed

--- src/utils/normaliser/skills.ts
+++ src/utils/normaliser/skills.ts
@@ -1,8 +1,6 @@
 import { IUserSkill } from 'interfaces/Ientities/IUserSkill';
 
 const extractCommonUserSkillInfo = (skills: IUserSkill[]) =>
-  skills.map((uSkill) => {
-    return { score: uSkill.score, skill: uSkill.skill };
-  });
+  skills.map((uSkill) => ({ score: uSkill.score, skill: uSkill.skill }));
 
 export { extractCommonUserSkillInfo };
