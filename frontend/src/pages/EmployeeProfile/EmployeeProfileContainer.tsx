dfbf8687d830a46074d9ea2b2e0ac71ce4b722c1
Julia <julia.maliutsina@gmail.com>
2022-03-21 23:10:44 +0300

    employee profile buttons

--- frontend/src/pages/EmployeeProfile/EmployeeProfileContainer.tsx
+++ frontend/src/pages/EmployeeProfile/EmployeeProfileContainer.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router';
 
 import { useGetEmployeeProfile } from 'api/employees';
+import { EMPLOYEE_INFO } from 'constants/employeeInfo';
 
 import ProfileContent from './EmployeeProfile';
 
 const EmployeeProfile: React.FC = () => {
   const params = useParams();
   const { data } = useGetEmployeeProfile(params.employeeId);
+
+  const [employeeInfo, setEmployeeInfo] = useState(EMPLOYEE_INFO.skills);
+  const [hoveredButton, setHoveredButton] = useState<string | undefined>(undefined);
+
+  const toggleEmployeeInfo = (infoToOpen: string) => {
+    setEmployeeInfo(infoToOpen);
+  };
+
+  const toggleHover = (buttonHovered: string) => {
+    setHoveredButton(buttonHovered === EMPLOYEE_INFO.none ? undefined : buttonHovered);
+  };
+
   return (
     <ProfileContent
       avatar={data?.avatar}
@@ -17,6 +30,10 @@ const EmployeeProfile: React.FC = () => {
       group={data?.group}
       phone={data?.phone}
       skype={data?.skype}
+      employeeInfo={employeeInfo}
+      toggleEmployeeInfo={toggleEmployeeInfo}
+      hoveredButton={hoveredButton}
+      toggleHover={toggleHover}
     />
   );
 };
--- frontend/src/pages/EmployeeProfile/styled.ts
+++ frontend/src/pages/EmployeeProfile/styled.ts
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
-import { List, ListItem } from '@mui/material';
+import { List, ListItem, Button as ToggleButton } from '@mui/material';
 
 import theme from 'themeSettings';
 import { Button } from 'components/Button';
+
+interface ToggleButtonProps {
+  isOpened: boolean;
+}
 
 export const ProfileBox = styled('div')({
   width: '90%',
@@ -62,4 +66,33 @@ export const BackButton = styled(Button)({
   [theme.breakpoints.down('sm')]: {
     display: 'none !important',
   },
+});
+export const SkillsAndCoursesBox = styled('div')({
+  marginLeft: '63px',
+  width: '80%',
+  display: 'flex',
+});
+export const SkillsAndCoursesButton = styled(ToggleButton)<ToggleButtonProps>(({ isOpened }) => ({
+  backgroundColor: '#ffffff !important',
+  color: '#000000 !important',
+  fontFamily: '"Ubuntu", sans-serif',
+  fontSize: '20px !important',
+  fontWeight: '400 !important',
+  width: '135px !important',
+  height: '40px',
+  borderRadius: '4px 4px 0px 0px !important',
+  border: '1px solid #0000001A !important',
+  padding: '0px !important',
+  ...(isOpened && {
+    width: '146px !important',
+    height: '44px',
+  }),
+}));
+export const AddCourseButton = styled(Button)({
+  width: '146px',
+  height: '40px',
+  margin: '-2px 0px 0px 500px !important',
+  fontWeight: '500',
+  fontSize: '20px !important',
+  padding: '0px !important',
 });
