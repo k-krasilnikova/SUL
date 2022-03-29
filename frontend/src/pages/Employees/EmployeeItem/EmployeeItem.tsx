7a3d6c8f35ed327b7bdf952c8c3841d7a4130cab
Irene Sinitsyna <hauteballe@gmail.com>
2022-03-29 15:56:33 +0300

    feat: added styles of employees page

--- frontend/src/pages/Employees/EmployeeItem/EmployeeItem.tsx
+++ frontend/src/pages/Employees/EmployeeItem/EmployeeItem.tsx
@@ -1,41 +1,55 @@
-import { Divider, Grid } from '@mui/material';
+import { Grid } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { UserAvatar } from 'components/Avatar';
 import { SIZE } from 'constants/sizes';
-import { IEmployee } from 'types/employee';
+import { PATHS } from 'constants/routes';
+import { IEmployeeItemProps } from 'types/employee';
 
-import { ImageWrapper, Position, UserContainer, UserName } from './styled';
-
-interface IEmployeeItemProps {
-  employee: IEmployee;
-}
+import {
+  Cell,
+  EmployeeContainer,
+  ImageWrapper,
+  InfoContainer,
+  Position,
+  StyledDivider,
+  Text,
+  UserName,
+} from './styled';
 
 const EmployeeItem: React.FC<IEmployeeItemProps> = ({ employee }) => (
-  <Grid item container xs={12}>
-    <Grid item xs={3}>
-      <ImageWrapper>
-        <UserAvatar size={SIZE.small} avatar={employee.avatar} />
-      </ImageWrapper>
-      <UserContainer>
-        <UserName>{`${employee.firstName} ${employee.lastName}`}</UserName>
-        <Position>{employee.position}</Position>
-      </UserContainer>
-    </Grid>
-    <Grid item xs={2}>
-      {employee.position}
-    </Grid>
-    <Grid item xs={2}>
-      {employee.group}
-    </Grid>
-    <Grid item xs={2}>
-      {employee.phone}
-    </Grid>
-    <Grid item xs={2}>
-      {employee.skype}
-    </Grid>
-    <Divider />
-  </Grid>
+  <>
+    <Link to={`${PATHS.employees}/${employee._id}`}>
+      <Grid item container xs={12} columnSpacing={2} wrap="nowrap">
+        <EmployeeContainer item xs={1} xl={2}>
+          <ImageWrapper>
+            <UserAvatar size={SIZE.small} avatar={employee.avatar} />
+          </ImageWrapper>
+          <InfoContainer>
+            <UserName>{`${employee.firstName} ${employee.lastName}`}</UserName>
+            <Position>{employee.position}</Position>
+          </InfoContainer>
+        </EmployeeContainer>
+        <Cell item xs={1}>
+          <Text>{employee.position}</Text>
+        </Cell>
+        <Cell item xs={1}>
+          <Text>Rank</Text>
+        </Cell>
+        <Cell item xs={1}>
+          <Text>{employee.group}</Text>
+        </Cell>
+        <Cell item xs={1}>
+          <Text>{employee.phone}</Text>
+        </Cell>
+        <Cell item xs={1}>
+          <Text>{employee.skype}</Text>
+        </Cell>
+      </Grid>
+      <StyledDivider />
+    </Link>
+  </>
 );
 
 export default EmployeeItem;
--- frontend/src/pages/Employees/styled.ts
+++ frontend/src/pages/Employees/styled.ts
@@ -1,9 +1,42 @@
+import { Grid } from '@mui/material';
 import styled from 'styled-components';
 
+import theme from 'themeSettings';
+
 export const EmployeesWrapper = styled('div')({
-  padding: '40px 30px',
-  fontFamily: 'Ubuntu',
+  padding: '41px 40px',
+  fontFamily: '"Ubuntu", sans-serif',
   height: 'calc(100% - 100px)',
   maxWidth: '1800px',
   width: '100%',
+  [theme.breakpoints.down('xl')]: {
+    padding: '16px',
+  },
+});
+
+export const HeaderText = styled('p')({
+  color: '#6c6c6c',
+  fontSize: '18px',
+  fontWeight: '400',
+  lineHeight: '21px',
+  margin: '0',
+  [theme.breakpoints.down('xl')]: {
+    fontSize: '16px',
+    lineHeight: '18px',
+  },
+});
+
+export const HeaderCell = styled(Grid)({
+  marginBottom: '30px !important',
+  minWidth: '200px',
+  [theme.breakpoints.down('xl')]: {
+    marginBottom: '26px !important',
+    minWidth: '130px',
+  },
+  [theme.breakpoints.up(1800)]: {
+    minWidth: '300px',
+  },
+  ':first-child': {
+    minWidth: '350px',
+  },
 });
