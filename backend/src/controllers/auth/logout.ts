cae915b66a5e776c9fdb6e6712a6516e09491060
zhuk <evgeniy.zhuk.by@gmail.com>
2022-04-01 15:29:09 +0300

    fix/V1-048 - added blacklist for tokens

--- backend/src/controllers/auth/logout.ts
+++ backend/src/controllers/auth/logout.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
-import { clearTokenProvider } from 'db/providers/authProvider';
+import { addTokenToBlackList, clearTokenProvider } from 'db/providers/authProvider';
 
 const logout = async (
   req: Request,
@@ -11,6 +11,12 @@ const logout = async (
     const { id: userId } = res.locals;
     await clearTokenProvider(userId);
     res.clearCookie('refreshToken');
+
+    const accessToken = req.headers.authorization?.split(' ')[1];
+    if (accessToken) {
+      await addTokenToBlackList(accessToken);
+    }
+
     res.json({ message: 'Logout is successful' });
   } catch (error) {
     next(error);
--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -4,13 +4,19 @@ import isExpectedHttpError from 'utils/typeGuards/isExpectedHttpError';
 import { verifyAccessToken } from 'utils/auth/authUtils';
 import UnauthorizedError from 'classes/errors/clientErrors/UnauthorizedError';
 import ForbiddenError from 'classes/errors/clientErrors/ForbiddenError';
+import { checkTokenInBlacklist } from '../db/providers/authProvider';
 
 const withAuth =
   (roles: Array<string>) => async (req: Request, res: Response, next: NextFunction) => {
     try {
       const accessToken = req.headers.authorization?.split(' ')[1];
 
-      if (!accessToken) {
+      let notValidToken;
+      if (accessToken) {
+        notValidToken = checkTokenInBlacklist(accessToken);
+      }
+
+      if (!accessToken || notValidToken) {
         throw new UnauthorizedError('No access token provided.');
       } else {
         const decodedToken = verifyAccessToken(accessToken);
