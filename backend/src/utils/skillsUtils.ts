7cfbe2f66ff98111699606a444710f8c055d79fe
Aliaksandr <aliaksandr@wse-050-71b.icx.local>
2022-04-01 16:16:10 +0300

    refactor: change fn calculate

--- backend/src/utils/skillsUtils.ts
+++ backend/src/utils/skillsUtils.ts
@@ -1,14 +1,18 @@
 import { ObjectId } from 'mongoose';
 
 import { getCommonSkill, getUserSkill, updateUserSkill } from 'db/providers/skillProvider';
+import { NOTHING } from 'config/constants';
 
-const calculatePoints = (courseComplexity: number, userScore: number) =>
-  courseComplexity > userScore && courseComplexity - userScore;
+const calculatePoints = (courseComplexity: number, userScore: number) => {
+  const isAdditionalScore = courseComplexity > userScore;
+  return isAdditionalScore ? courseComplexity - userScore : NOTHING;
+};
 
 export const addPointToUserSkill =
   (complexity: number, userId: string) => async (commonSkillId: string | ObjectId) => {
     const { score } = await getUserSkill(userId, commonSkillId);
     const { maxScore } = await getCommonSkill(commonSkillId);
+    const isSkillIncomplete = score < maxScore;
     const points = calculatePoints(complexity, score);
-    return points && score < maxScore && updateUserSkill(userId, points, commonSkillId);
+    return isSkillIncomplete && updateUserSkill(userId, points, commonSkillId);
   };
