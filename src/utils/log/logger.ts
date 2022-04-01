4f0d05d4abab570aca4b042b55c99c2a37624309
dapodev <apolonik.danila2002@gmail.com>
2022-04-01 17:24:53 +0300

    feat: updated logger creating and error handling middleware

--- src/utils/log/logger.ts
+++ src/utils/log/logger.ts
@@ -2,22 +2,29 @@ import winston from 'winston';
 
 import { ENVIROMENTS } from 'config/constants';
 
-const logFormat = winston.format.printf(({ message, level }) => `${level}:${message}`);
+const logFormat = winston.format.printf(({ message, level }) => `${level}: ${message}`);
 
-winston.loggers.add(ENVIROMENTS.qa, {
+const qaLoggerOptions = {
   format: winston.format.combine(
     winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     winston.format.colorize(),
     logFormat,
   ),
   transports: [new winston.transports.Console()],
-});
+};
 
-winston.loggers.add(ENVIROMENTS.prod, {
+const prodLoggerOptions = {
   format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.uncolorize(),
+    winston.format.colorize(),
     winston.format.json(),
   ),
   transports: [new winston.transports.Console()],
-});
+};
+
+const logger =
+  process.env.NODE_ENV === ENVIROMENTS.qa
+    ? winston.createLogger(qaLoggerOptions)
+    : winston.createLogger(prodLoggerOptions);
+
+export default logger;
