fdeaf89c8240124871c2c72e44fb51243eae8999
Aliaksandr <dfdf1534@gmail.com>
2022-04-04 18:48:20 +0300

    refactor: replace complexity with tech score

--- src/utils/dto/skillsDto.ts
+++ src/utils/dto/skillsDto.ts
@@ -1,12 +1,11 @@
-import { ObjectId } from 'mongoose';
-
 import { IUserSkill } from 'interfaces/Ientities/IUserSkill';
 import { containsObjectId } from 'utils/comparator/ObjectId/containsObjectId';
+import { ICourse } from 'interfaces/Ientities/Icourses';
 
-const specifySkills = (userSkills: IUserSkill[], techArrayIds: ObjectId[]) => {
+const specifySkills = (userSkills: IUserSkill[], techArrayIds: ICourse['technologies']) => {
   const skillIds = userSkills.map((userSkill) => userSkill.skill);
-  const newSkills = techArrayIds.filter((techId) => !containsObjectId(skillIds, techId));
-  const oldSkills = techArrayIds.filter((techId) => containsObjectId(skillIds, techId));
+  const newSkills = techArrayIds.filter((techId) => !containsObjectId(skillIds, techId.skill));
+  const oldSkills = techArrayIds.filter((techId) => containsObjectId(skillIds, techId.skill));
   return { newSkills, oldSkills };
 };
 
