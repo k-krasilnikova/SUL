38958c18a3523631ee2218db1eeb8f901a927cf4
dapodev <apolonik.danila2002@gmail.com>
2022-04-04 16:20:15 +0300

    feat: implemented transformRoute function

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -15,6 +15,7 @@ export const PATHS: {
   learnClientCourse: string;
   courseTest: string;
   skillsMap: string;
+  id: string;
 } = {
   home: '/',
   profile: '/profile',
@@ -32,6 +33,7 @@ export const PATHS: {
   learnClientCourse: 'learn/:courseId',
   courseTest: 'learn/:courseId/test',
   skillsMap: '/skills-map',
+  id: ':id',
 };
 
 export const API: {
