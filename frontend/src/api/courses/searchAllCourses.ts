19eda552f84c1896d89c322cfb1a92662650b713
zhuk <evgeniy.zhuk.by@gmail.com>
2022-04-06 17:21:27 +0300

    fix/V1-177 - added constants

--- frontend/src/api/courses/searchAllCourses.ts
+++ frontend/src/api/courses/searchAllCourses.ts
@@ -4,9 +4,10 @@ import { AxiosError } from 'axios';
 import { apiClientWrapper } from 'api/base';
 import { API } from 'constants/routes';
 import { Course } from 'types/course';
+import { QUERY_KEYS } from '../../constants/queryKeyConstants';
 
 const useSearchAllCourses = (courseName: string): UseQueryResult<Array<Course>, AxiosError> => {
-  return useQuery(['courses', courseName], async () => {
+  return useQuery([QUERY_KEYS.coursesRequests, courseName], async () => {
     const apiClient = apiClientWrapper();
     const courseNameEncoded = encodeURIComponent(courseName);
     const response = await apiClient.get(`${API.getCourses}?title=${courseNameEncoded}`);
--- frontend/src/api/myCourses/searchClientCourses.ts
+++ frontend/src/api/myCourses/searchClientCourses.ts
@@ -4,9 +4,10 @@ import { AxiosError } from 'axios';
 import { apiClientWrapper } from 'api/base';
 import { API } from 'constants/routes';
 import { ClientCourse } from 'types/clientCourse';
+import { QUERY_KEYS } from 'constants/queryKeyConstants';
 
 const useSearchClientCourses = (): UseQueryResult<Array<ClientCourse>, AxiosError> => {
-  return useQuery('search', async () => {
+  return useQuery(QUERY_KEYS.myCourses, async () => {
     const apiClient = apiClientWrapper();
     const response = await apiClient.get(`${API.getMyCourses}`);
     const searchResponse = response.data;
