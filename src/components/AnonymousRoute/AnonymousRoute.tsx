3a04fad84fade4fa74d160036089699a7e60a486
zhuk <evgeniy.zhuk.by@gmail.com>
2022-04-07 12:59:16 +0300

    fix/V1-048 - intermediate changes

--- src/components/AnonymousRoute/AnonymousRoute.tsx
+++ src/components/AnonymousRoute/AnonymousRoute.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 import { PATHS } from 'constants/routes';
-import { getAuthResponseData } from 'utils/helpers/getAuthResponseData';
+import { getAuthResponseData, TokenTypes } from 'utils/helpers/getAuthResponseData';
 
 interface IAnonymousRoute {
   children: React.ReactNode;
 }
 
 const AnonymousRoute: React.FC<IAnonymousRoute> = ({ children }) => {
-  const accessToken = getAuthResponseData();
+  const accessToken = getAuthResponseData(TokenTypes.accessToken);
   return accessToken ? <Navigate to={PATHS.profile} /> : <>{children}</>;
 };
 
