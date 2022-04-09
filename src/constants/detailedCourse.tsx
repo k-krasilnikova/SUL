258767c3d9a455463ecc3d78b84de3fc43b0882f
PechurinaLena <elena-pechurina@itechart-group.com>
2022-04-09 18:32:31 +0300

    Added const for checking description length

--- src/constants/detailedCourse.tsx
+++ src/constants/detailedCourse.tsx
@@ -1,4 +1,3 @@
-export const OPEN_FULL_TEXT = 'Look in full';
 export const COMPLETED_STATUS_TEXT = 'Completed';
 export const FAILED_STATUS_TEXT = 'Failed';
 export const PROGRESS_COLOR = '#131313';
