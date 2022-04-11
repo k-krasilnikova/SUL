25f05358785304fd5305f51b9b2e1563f3a625e9
dapodev <apolonik.danila2002@gmail.com>
2022-04-11 16:43:16 +0300

    feat: materials models refactored

--- backend/src/interfaces/Ientities/Icourses.ts
+++ backend/src/interfaces/Ientities/Icourses.ts
@@ -1,6 +1,7 @@
 import { ObjectId } from 'mongoose';
 
 import { UserRank } from 'enums/users';
+import { MaterialContentType } from 'enums/materials';
 
 interface ICourse {
   _id?: ObjectId;
@@ -9,9 +10,14 @@ interface ICourse {
   requiredSkills?: ObjectId[];
   complexity: UserRank;
   description: string;
-  materials: { stage: number; content: Array<string> }[];
+  materials: { stage: number; content: Array<IContentElement> }[];
   test: ObjectId;
   avatar?: string;
+}
+
+interface IContentElement {
+  type: MaterialContentType;
+  material: string;
 }
 
 type TCourses = Array<ICourse>;
