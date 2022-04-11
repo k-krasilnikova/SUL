a2bccc6e276c59ca17433ff428651aa1e6271610
dapodev <apolonik.danila2002@gmail.com>
2022-04-11 16:43:16 +0300

    feat: materials models refactored

--- src/db/models/Course.ts
+++ src/db/models/Course.ts
@@ -1,6 +1,7 @@
 import { Schema, model } from 'mongoose';
 
 import { ICourse } from 'interfaces/Ientities/Icourses';
+import { MaterialContentType } from 'enums/materials';
 
 const courseSchema = new Schema<ICourse>({
   title: { type: String, required: true, unique: true },
@@ -10,7 +11,24 @@ const courseSchema = new Schema<ICourse>({
   ],
   requiredSkills: [{ type: Schema.Types.ObjectId }],
   complexity: { type: Number, required: true },
-  materials: [{ stage: { type: Number }, content: [{ type: String }] }],
+  materials: [
+    {
+      stage: { type: Number },
+      content: [
+        {
+          type: {
+            type: String,
+            required: true,
+            default: MaterialContentType.plain,
+          },
+          material: {
+            type: String,
+            required: true,
+          },
+        },
+      ],
+    },
+  ],
   test: { type: Schema.Types.ObjectId, ref: 'Test' },
 });
 
