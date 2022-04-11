0987a183d4fe6ca0387ec1dbcde807f637d6b15e
dapodev <apolonik.danila2002@gmail.com>
2022-04-11 12:04:23 +0300

    fix: fixed percentage issue and empty result error

--- src/utils/normaliser/tests.ts
+++ src/utils/normaliser/tests.ts
@@ -1,12 +1,13 @@
-import { TWO_DIGITS } from 'config/constants';
+import { PERCENTS, TWO_DIGITS } from 'config/constants';
 import { IClientCourse } from 'interfaces/Ientities/IclientCourses';
 import { ITestResultResponse } from 'interfaces/IResponse/IResponse';
 
 const normaliseTestResult = (testResult: IClientCourse['testResult']): ITestResultResponse => {
   const answers = testResult.map((question) => ({ qN: question.qN, isCorrect: question.aN }));
-  const percentage = parseFloat(
-    (answers.filter((answer) => answer.isCorrect).length / answers.length).toFixed(TWO_DIGITS),
-  );
+  const percentage =
+    parseFloat(
+      (answers.filter((answer) => answer.isCorrect).length / answers.length).toFixed(TWO_DIGITS),
+    ) * PERCENTS;
 
   const response: ITestResultResponse = { answers, percentage };
 
