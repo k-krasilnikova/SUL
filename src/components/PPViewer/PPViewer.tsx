3052ec81c906e12e8fc6f8dd4dd14b72dd6f33cc
dapodev <apolonik.danila2002@gmail.com>
2022-04-13 10:50:10 +0300

    refactor: PPviewer code refactored

--- src/components/PPViewer/PPViewer.tsx
+++ src/components/PPViewer/PPViewer.tsx
@@ -5,16 +5,13 @@ interface IPPViewer {
   link: string;
 }
 
+const viewerContainerStyles = {
+  width: '100%',
+  border: 'none',
+} as const;
+
 const PPViewer: React.FC<IPPViewer> = ({ link }) => {
-  return (
-    <GoogleSlides
-      slidesLink={link}
-      containerStyle={{
-        width: '100%',
-        border: 'none',
-      }}
-    />
-  );
+  return <GoogleSlides slidesLink={link} containerStyle={viewerContainerStyles} />;
 };
 
 export default PPViewer;
