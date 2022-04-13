6118aa11cc71f66ed0a10c256fd5ef7ba2e738a6
dapodev <apolonik.danila2002@gmail.com>
2022-04-13 10:50:10 +0300

    refactor: PPviewer code refactored

--- frontend/src/components/PPViewer/PPViewerContainer.tsx
+++ frontend/src/components/PPViewer/PPViewerContainer.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
+
 import PPViewer from './PPViewer';
 
 interface IPPViewerContainer {
   link: string;
 }
 
-const PPViewerContainer: React.FC<IPPViewerContainer> = ({ link }) => {
-  return <PPViewer link={link} />;
-};
+const PPViewerContainer: React.FC<IPPViewerContainer> = ({ link }) => <PPViewer link={link} />;
 
 export default PPViewerContainer;
