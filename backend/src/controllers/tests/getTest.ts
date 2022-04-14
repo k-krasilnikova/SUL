e304a48a81ca9a6897f8010c55a8243375f7046d
dapodev <apolonik.danila2002@gmail.com>
2022-04-14 14:59:50 +0300

    [BE]feat: implemented new util to check dates, refactored code of getTest controller

--- backend/src/controllers/tests/getTest.ts
+++ backend/src/controllers/tests/getTest.ts
@@ -1,29 +1,31 @@
 import { NextFunction, Request, Response } from 'express';
+import { isEmpty } from 'lodash';
 
 import NotFoundError from 'classes/errors/clientErrors/NotFoundError';
-import { getTestProvider } from 'db/providers/testProvider';
-import { checkTestDate } from 'utils/validation/checkTestDate';
-import CourseStatus from 'enums/coursesEnums';
 import BadRequestError from 'classes/errors/clientErrors/BadRequestError';
+import { getTestProvider } from 'db/providers/testProvider';
 import { getClientCourseProvider } from 'db/providers/clientCourseProvider';
+import CourseStatus from 'enums/coursesEnums';
+import { isTestAvailableByDate } from 'utils/validation/tests';
 
 const getTest = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id: clientCourseId } = req.params;
-    const { status: courseStatus, testDate } = await getClientCourseProvider(clientCourseId);
+    const { status: courseStatus, finishTestDate } = await getClientCourseProvider(clientCourseId);
 
     if (courseStatus !== CourseStatus.testing) {
       throw new BadRequestError(`Failed: can not get test with status: ${courseStatus}.`);
     }
 
-    if (testDate && !checkTestDate(testDate)) {
+    if (!isTestAvailableByDate(finishTestDate)) {
       throw new BadRequestError('Your test is disabled.');
     }
 
     const test = await getTestProvider(clientCourseId);
-    if (!test.length) {
+    if (isEmpty(test)) {
       throw new NotFoundError('No tests found.');
     }
+
     res.json(test);
   } catch (err) {
     next(err);
