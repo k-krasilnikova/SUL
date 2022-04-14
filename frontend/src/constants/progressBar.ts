c589bbcb9fa8d630600230324fd24cd88bf93645
Irene Sinitsyna <hauteballe@gmail.com>
2022-04-14 16:02:40 +0300

    fix: fixed progressBar styles on Employee profile courses

--- frontend/src/constants/progressBar.ts
+++ frontend/src/constants/progressBar.ts
@@ -1,10 +1,13 @@
 export const PROGRESS_COLOR = '#1BC02C';
 export const TEXT_COLOR = '#9C9C9C';
+export const TRAIL_COLOR = '#D6D6D6';
 
 export const VARIANTS = {
   notStarted: 'notStarted',
+  employeeNotStarted: 'employeeNotStarted',
   completed: 'completed',
   failed: 'failed',
+  employeeFailed: 'employeeFailed',
   successful: 'successful',
   failedWithPercentage: 'failedWithPercentage',
   successfulWithPercentage: 'successfulWithPercentage',
