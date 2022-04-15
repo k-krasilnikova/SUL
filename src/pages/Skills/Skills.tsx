e8cd64244dda9496961bc9fb53ece1f9afedc32e
PechurinaLena <elena-pechurina@itechart-group.com>
2022-04-15 17:21:58 +0300

    Changed components [Intermediate]

--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -2,40 +2,82 @@ import React, { Suspense } from 'react';
 
 import { AuthorizedLayout } from 'components/Layout';
 import Loader from 'components/Loader';
+import SkillItem from 'components/Skill';
+import { NoContent } from 'components/NoContent';
+
+import { LOADER } from 'constants/loaderTypes';
+import { NO_SKILLS } from 'constants/messages';
+
 import { ISkills } from 'types/skill';
 import { ResponseDataType } from 'types/responseData';
-import { LOADER } from 'constants/loaderTypes';
-import { NoContent } from 'components/NoContent';
-import { NO_SKILLS } from 'constants/messages';
-import { MobileSearch } from 'components/Layout/MobileSearch';
-import SkillItem from 'components/Skill';
+
+import InputAdornment from '@material-ui/core/InputAdornment';
+import { Search } from '@mui/icons-material';
 import {
   SkillsPageContainer,
-  MobileSearchWrapper,
+  SearchSkill,
+  SearchWrapper,
   SkillsWrapper,
   SkillsBox,
   SkillsTitle,
+  StyledDivider,
 } from './styled';
 
-type SkillsPageProps = ISkills & ResponseDataType;
+export interface IProps {
+  searchSkillInList: (value: string) => void;
+  checkSpace: (event: React.KeyboardEvent) => void;
+  checkPastedValue: (value: string) => void;
+  searchSkill: string;
+}
 
-const Skills: React.FC<SkillsPageProps> = ({ skills, isLoading }) => {
+type SkillsPageProps = ISkills & IProps & ResponseDataType;
+
+const Skills: React.FC<SkillsPageProps> = ({
+  skills,
+  isLoading,
+  checkSpace,
+  searchSkillInList,
+  checkPastedValue,
+  searchSkill,
+}) => {
   return (
     <AuthorizedLayout pageName="Skills">
       {isLoading ? (
         <Loader color="primary" type={LOADER.content} />
       ) : skills?.length ? (
         <SkillsPageContainer container>
-          <MobileSearchWrapper>
-            <MobileSearch />
-          </MobileSearchWrapper>
           <SkillsWrapper>
+            <SearchWrapper>
+              <SearchSkill
+                disableUnderline
+                placeholder="Search"
+                inputProps={{ maxLength: 100 }}
+                fullWidth
+                startAdornment={
+                  <InputAdornment position="start">
+                    <Search color="disabled" fontSize="medium" />
+                  </InputAdornment>
+                }
+                onKeyDown={(event) => {
+                  checkSpace(event);
+                }}
+                onChange={(event) => {
+                  searchSkillInList(event.target.value);
+                }}
+                onPaste={(event) => {
+                  event.preventDefault();
+                  checkPastedValue(event.clipboardData.getData('Text'));
+                }}
+                value={searchSkill}
+              />
+              <StyledDivider />
+            </SearchWrapper>
             {skills?.map((SkillGroup) => (
               <Suspense fallback={<Loader color="primary" type={LOADER.component} />}>
                 <SkillsTitle>{SkillGroup.name}</SkillsTitle>
                 <SkillsBox>
                   {SkillGroup.skills.map((Skill) => (
-                    <SkillItem name={Skill.name} image={Skill.image} />
+                    <SkillItem name={Skill.name} skillImage={Skill.image} />
                   ))}
                 </SkillsBox>
               </Suspense>
