faf6bd2d072916759b67a7f8d6a96deaa3debfbd
PechurinaLena <elena-pechurina@itechart-group.com>
2022-04-17 19:08:44 +0300

    Implemented search function for Skill page for Admin

--- frontend/src/api/skills/index.ts
+++ frontend/src/api/skills/index.ts
@@ -1,1 +1,2 @@
 export { default as useGetSkills } from './getAllSkills';
+export { default as useSearchSkills } from './searchSkills';
--- frontend/src/pages/Skills/SkillsContainer.tsx
+++ frontend/src/pages/Skills/SkillsContainer.tsx
@@ -1,38 +1,60 @@
-import React, { useState } from 'react';
+import { FC, useState, ChangeEvent, KeyboardEvent, ClipboardEvent, useEffect } from 'react';
 
-import { useGetSkills } from 'api/skills';
+import { useGetSkills, useSearchSkills } from 'api/skills';
+import { checkWhitespace, formatInputValue } from 'utils/helpers/searchHelpers';
+import { useDebounce } from 'hooks';
+
+import { SkillsList } from 'types/skill';
 import Skills from './Skills';
 
-const SkillsContainer: React.FC = () => {
-  const [searchSkill, setSearchSkill] = useState('');
+const SkillsContainer: FC = () => {
+  const [searchInputValue, setSearchInputValue] = useState<string>('');
+  const [skillFounded, setSkillFounded] = useState<Array<SkillsList>>([]);
 
+  const debouncedSearchValue = useDebounce(searchInputValue);
+  const { data: skillSearch } = useSearchSkills(debouncedSearchValue);
   const { data: skills } = useGetSkills();
 
-  const searchSkillInList = (value: string) => {
-    const formattedValue = value.split(/\s+/).join(' ').trimStart();
-    setSearchSkill(formattedValue);
-  };
-
-  const checkPastedValue = (value: string) => {
-    const formattedValue = value.split(/\s+/).join(' ').trimStart().trimEnd();
-    setSearchSkill(formattedValue);
-  };
-
-  const checkSpace = (event: React.KeyboardEvent) => {
-    const { key } = event;
-    const formattedKey = key.trim();
-    if (!formattedKey && searchSkill.length === 0) {
-      event.preventDefault();
+  useEffect(() => {
+    if (debouncedSearchValue) {
+      if (
+        skillSearch?.filter((skillList) => {
+          return skillList.skills.filter((skill) =>
+            skill.name.toLowerCase().includes(searchInputValue.toLowerCase()),
+          );
+        })
+      ) {
+        setSkillFounded(skillSearch);
+      }
+    } else {
+      setSkillFounded([]);
     }
+  }, [debouncedSearchValue, searchInputValue, skillSearch]);
+
+  const searchSkills = (event: ChangeEvent<HTMLInputElement>): void => {
+    const formattedValue = formatInputValue(event.target.value);
+    setSearchInputValue(formattedValue);
+  };
+
+  const checkSpace = (event: KeyboardEvent) => {
+    checkWhitespace(event, searchInputValue);
+  };
+
+  const checkPastedValue = (event: ClipboardEvent) => {
+    event.preventDefault();
+    const inputValue = event.clipboardData.getData('Text');
+    const formattedValue = inputValue.split(/\s+/).join(' ').trimStart().trimEnd();
+    setSearchInputValue(formattedValue);
   };
 
   return (
     <Skills
       skills={skills}
-      searchSkillInList={searchSkillInList}
+      searchSkills={searchSkills}
+      searchInputValue={searchInputValue}
       checkSpace={checkSpace}
       checkPastedValue={checkPastedValue}
-      searchSkill={searchSkill}
+      skillFounded={skillFounded}
     />
   );
 };
