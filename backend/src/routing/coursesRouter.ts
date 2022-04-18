61bbd48d3904accccdb62946c762076fa9674b2f
zhuk <evgeniy.zhuk.by@gmail.com>
2022-04-18 14:00:34 +0300

    fix/V1-163 - add course

--- backend/src/routing/coursesRouter.ts
+++ backend/src/routing/coursesRouter.ts
@@ -12,6 +12,7 @@ import adapterUser from 'controllers/user/adapterUser';
 import adapterSender from 'controllers/pendingCourses/adapterSender';
 import deleteCourse from 'controllers/admin/deleteCourse';
 import editCourse from 'controllers/admin/editCourse';
+import addCourse from 'controllers/admin/addCourse';
 
 const coursesRouter = Router();
 coursesRouter.get(
@@ -38,6 +39,8 @@ coursesRouter.get(
   getAllCourses,
 );
 coursesRouter.put(SubRoutes.updateCourse, withAuth([USER_ROLES.ADMIN]), editCourse, adapterSender);
+
+coursesRouter.post(SubRoutes.createCourse, withAuth([USER_ROLES.ADMIN]), addCourse, adapterSender);
 
 coursesRouter.delete(`${SubRoutes.deleteCourse}`, withAuth([USER_ROLES.ADMIN]), deleteCourse);
 
