824851c20b3928ca041c5c8ac5948db5db464491
zhuk <evgeniy.zhuk.by@gmail.com>
2022-04-19 15:05:25 +0300

    fix/V1-163 - fixed client courses re-fetching when searching courses

--- src/api/myCourses/searchClientCourses.ts
+++ src/api/myCourses/searchClientCourses.ts
@@ -6,13 +6,18 @@ import { API } from 'constants/routes';
 import { IClientCourse } from 'types/clientCourse';
 import { QUERY_KEYS } from 'constants/queryKeyConstants';
 
+
 const useSearchClientCourses = (): UseQueryResult<Array<IClientCourse>, AxiosError> => {
-  return useQuery(QUERY_KEYS.myCourses, async () => {
-    const apiClient = apiClientWrapper();
-    const response = await apiClient.get(`${API.getMyCourses}`);
-    const searchResponse = response.data;
-    return searchResponse;
-  });
+  return useQuery(
+    QUERY_KEYS.myCourses,
+    async () => {
+      const apiClient = apiClientWrapper();
+      const response = await apiClient.get(`${API.getMyCourses}`);
+      const searchResponse = response.data;
+      return searchResponse;
+    },
+    { cacheTime: 900000 },
+  );
 };
 
 export default useSearchClientCourses;
