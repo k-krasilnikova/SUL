c5df9167a8bd619574ef88eee58a652648936130
Aliaksandr <aliaksandr@wse-050-71b.icx.local>
2022-04-20 18:31:33 +0300

    feat: add some exercise to migrations

--- src/interfaces/Ientities/Icourses.ts
+++ src/interfaces/Ientities/Icourses.ts
@@ -9,7 +9,16 @@ interface ICourse {
   requiredSkills?: ObjectId[];
   complexity: UserRank;
   description: string;
-  materials: { stage: number; content: Array<string> }[];
+  materials: {
+    stage: number;
+    content: Array<string>;
+    exercise?: {
+      eN: number;
+      title: string;
+      task: string;
+      code: string;
+    };
+  }[];
   test: ObjectId;
   avatar: string;
 }
