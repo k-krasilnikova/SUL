fedee3c7bc63962c8bbf87d1ef4328086bfd8e68
dapodev <apolonik.danila2002@gmail.com>
2022-04-20 11:00:26 +0300

    feat: implemented route and controller

--- backend/src/controllers/courses/getCoursesMap.ts
+++ backend/src/controllers/courses/getCoursesMap.ts
@@ -1,7 +1,10 @@
-import { getUserProvider } from 'db/providers/userProvider';
 import { NextFunction, Request, Response } from 'express';
+
+import { getCoursesByIdsWithStatusProvider } from 'db/providers/courseProvider';
+import { getUserProvider, getUserStackProvider } from 'db/providers/userProvider';
 import { ICoursesMapResponse } from 'interfaces/IResponse/IResponse';
 import { getStackCoursesUniqueIds } from 'utils/normaliser/stack';
+import { addMissingCoursesMapElements, generateCoursesMapResponse } from 'utils/normaliser/courses';
 
 const getCoursesMap = async (
   req: Request,
@@ -11,9 +14,18 @@ const getCoursesMap = async (
   try {
     const { id: userId } = res.locals;
 
-    const { rank: userRank, stack: userStack } = await getUserProvider(userId);
+    const { rank: userRank } = await getUserProvider(userId);
+    const userStack = await getUserStackProvider(userId);
 
-    const courseIds = getStackCoursesUniqueIds(userStack);
+    const coursesIds = getStackCoursesUniqueIds(userStack);
+
+    const relatedCourses = await getCoursesByIdsWithStatusProvider(coursesIds, userId);
+
+    const responseCascade = generateCoursesMapResponse(relatedCourses, userRank);
+
+    const filledResponse = addMissingCoursesMapElements(responseCascade);
+
+    res.locals.results = filledResponse;
 
     next();
   } catch (error) {
