b8d289ac256620e10f99a969d3017db7f0159375
dapodev <apolonik.danila2002@gmail.com>
2022-04-21 17:43:04 +0300

    refactor: materials validation refactored

--- backend/src/utils/validation/isValidMaterials.ts
+++ backend/src/utils/validation/isValidMaterials.ts
@@ -1,4 +1,11 @@
+import { MaterialContentType } from 'enums/materials';
 import { IUpdateCourseBody } from 'interfaces/ICourses/IQueryCourses';
+import isValidTitle from './isValidTitle';
+
+const isValidContentType = (type: MaterialContentType): boolean => {
+  const existingTypeValues = Object.values(MaterialContentType);
+  return existingTypeValues.some((typeValue) => typeValue === type);
+};
 
 const isValidMaterials = (materials: IUpdateCourseBody['materials']): boolean => {
   if (!materials?.length) {
@@ -10,7 +17,10 @@ const isValidMaterials = (materials: IUpdateCourseBody['materials']): boolean =>
       Array.isArray(material.content) &&
         material.content.length &&
         material.content.every(
-          (contentElement) => contentElement && typeof contentElement === 'string',
+          (contentElement) =>
+            contentElement &&
+            isValidContentType(contentElement.type) &&
+            isValidTitle(contentElement.material),
         ),
     ),
   );
