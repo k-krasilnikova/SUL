29bd2135bc2f501c8346917e216e2cbe351002fa
Ostapenlo Andrei <ostapdans99@gmail.com>
2022-04-23 11:21:35 +0400

    added: added sorting for courses and missing keys

--- frontend/src/pages/SkillsMap/StackMap/CourseMapForCurrentStack/CoursesList/CoursesList.tsx
+++ frontend/src/pages/SkillsMap/StackMap/CourseMapForCurrentStack/CoursesList/CoursesList.tsx
@@ -1,17 +1,17 @@
 import { FC } from 'react';
 
 import { NO_COURSES } from 'constants/messages';
-import { ICoursesProps } from 'pages/SkillsMap/types';
+import { TCoursesListProps } from 'pages/SkillsMap/types';
 
 import Course from './Course';
 import { NoCoursesText } from './styled';
 
-const Courses: FC<ICoursesProps> = ({ courses }) => (
+const CoursesList: FC<TCoursesListProps> = ({ courses }) => (
   <>
     {courses.length ? (
       <>
         {courses.map((course) => (
-          <Course course={course} />
+          <Course key={course._id} course={course} />
         ))}
       </>
     ) : (
@@ -20,4 +20,4 @@ const Courses: FC<ICoursesProps> = ({ courses }) => (
   </>
 );
 
-export default Courses;
+export default CoursesList;
--- frontend/src/pages/SkillsMap/StackMap/CourseMapForCurrentStack/CoursesList/index.tsx
+++ frontend/src/pages/SkillsMap/StackMap/CourseMapForCurrentStack/CoursesList/index.tsx
@@ -1,1 +1,1 @@
-export { default } from './CoursesList';
+export { default } from './CoursesListContainer';
