fce2338663aa6f38726dc21eb34f57cfd3668614
PechurinaLena <elena-pechurina@itechart-group.com>
2022-04-23 11:06:22 +0300

    Refactored according to FE style guide

--- frontend/src/components/Routes/RoleRoute.tsx
+++ frontend/src/components/Routes/RoleRoute.tsx
@@ -3,14 +3,17 @@ import { Navigate } from 'react-router-dom';
 
 import { useGetProfile } from 'api/profile';
 import { Role } from 'constants/menuRoles';
+import { PATHS } from 'constants/routes';
+import { Numbers } from 'enums/numbers';
+
 import { IRoleRouteProps } from './types';
 
 const RoleRoute: FC<IRoleRouteProps> = ({ children, roles }) => {
   const { data: userProfileResponse } = useGetProfile();
   const isAllowed = roles?.includes(userProfileResponse?.role as Role);
-  const isPageReloaded = window.performance && window.performance.navigation.type === 1;
+  const isPageReloaded = window.performance && window.performance.navigation.type === Numbers.one;
 
-  return isAllowed || isPageReloaded ? <>{children}</> : <Navigate replace to="/not-found" />;
+  return isAllowed || isPageReloaded ? <>{children}</> : <Navigate replace to={PATHS.notFound} />;
 };
 
 export default RoleRoute;
