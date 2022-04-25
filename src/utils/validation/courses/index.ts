3700e82f74853c3e6c58c8de78dab447fe1e049c
dapodev <apolonik.danila2002@gmail.com>
2022-04-25 15:31:15 +0300

    refactor: materials validation refactored

--- src/utils/validation/courses/index.ts
+++ src/utils/validation/courses/index.ts
@@ -1,19 +1,21 @@
 import isValidAvatar from './isValidAvatar';
 import isValidComplexity from './isValidComplexity';
 import isValidCourseData from './isValidCourseData';
-import isValidMaterials from './isValidMaterials';
+import validateMaterials from './isValidMaterials';
 import isValidTechnologies from './isValidTechnologies';
 import isValidTest from './isValidTest';
 import isValidText from './isValidText';
 import validateTitle from './validateTitle';
+import validatePlainMaterial from './validatePlainMaterial';
 
 export {
   isValidAvatar,
   isValidComplexity,
   isValidCourseData,
-  isValidMaterials,
+  validateMaterials,
   isValidTechnologies,
   isValidTest,
   isValidText,
   validateTitle,
+  validatePlainMaterial,
 };
