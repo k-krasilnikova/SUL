90b1e8344c7932e9c4a8de4b1da412973ab2be4b
PechurinaLena <elena-pechurina@itechart-group.com>
2022-04-26 15:27:39 +0300

    Fixed search for mobile

--- frontend/src/api/courses/searchAllCourses.ts
+++ frontend/src/api/courses/searchAllCourses.ts
@@ -6,15 +6,14 @@ import { QUERY_KEYS } from 'constants/queryKeyConstants';
 import { API } from 'constants/routes';
 import { ICourse } from 'types/course';
 
-const useSearchAllCourses = (courseName: string): UseQueryResult<Array<ICourse>, AxiosError> => {
+const useSearchAllCourses = (courseName: string): UseQueryResult<ICourse[], AxiosError> => {
   return useQuery(
     [QUERY_KEYS.coursesRequests, courseName],
     async () => {
       const apiClient = apiClientWrapper();
       const courseNameEncoded = encodeURIComponent(courseName);
       const response = await apiClient.get(`${API.courses}?title=${courseNameEncoded}`);
-      const searchResponse = response.data;
-      return searchResponse;
+      return response.data;
     },
     {
       enabled: !!courseName,
