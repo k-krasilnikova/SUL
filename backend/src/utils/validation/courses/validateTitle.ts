226e2ff6b3e18b7f33471841669f24f0747e73f3
dapodev <apolonik.danila2002@gmail.com>
2022-04-26 13:11:37 +0300

    refactor: refactored title validation

--- backend/src/utils/validation/courses/validateTitle.ts
+++ backend/src/utils/validation/courses/validateTitle.ts
@@ -1,20 +1,8 @@
-import { isNotNumbersOnly, isNotSpecialsOnly } from '../strings';
-import { convertToTypeUnsafe } from '../../typeConversion/common';
 import { TitleValidation } from '../schemas/courses';
-import capitalizeFirstLetter from '../../string/capitalizeFirstLetter';
-import fullTrim from '../../string/fullTrim';
 
 const validateTitle = (title?: string): string | null => {
   try {
-    const partlyValidatedTitle = TitleValidation.validateSync(title);
-
-    const titleValue = capitalizeFirstLetter(
-      fullTrim(convertToTypeUnsafe<string>(partlyValidatedTitle)),
-    );
-
-    const isValidated = isNotNumbersOnly(titleValue) && isNotSpecialsOnly(titleValue);
-
-    return isValidated ? titleValue : null;
+    return TitleValidation.validateSync(title) || null;
   } catch {
     return null;
   }
--- backend/src/utils/validation/schemas/courses.ts
+++ backend/src/utils/validation/schemas/courses.ts
@@ -1,11 +1,18 @@
+import capitalizeFirstLetter from 'utils/string/capitalizeFirstLetter';
+import fullTrim from 'utils/string/fullTrim';
 import { string, StringSchema } from 'yup';
 
 import { MAX_TITLE_LENGTH, MIN_TITLE_LENGTH } from '../courses/constants';
+import { isNotNumbersOnly, isNotSpecialsOnly } from '../strings';
 
 const TitleValidation: StringSchema = string()
   .required()
   .max(MAX_TITLE_LENGTH)
   .min(MIN_TITLE_LENGTH)
-  .trim();
+  .trim()
+  .test(isNotNumbersOnly)
+  .test(isNotSpecialsOnly)
+  .transform(fullTrim)
+  .transform(capitalizeFirstLetter);
 
 export { TitleValidation };
