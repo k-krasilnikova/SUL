e8905e6b7a99355273e7d524c6279f86b370afb9
Aliaksandr <aliaksandr@wse-050-71b.icx.local>
2022-04-28 14:40:42 +0300

    feat: add env for local database

---
+++ backend/migrate-mongo-local-config.js
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const config = {
+  mongodb: {
+    url: process.env.DATABASE_LOCAL_URL,
+
+    options: {
+      useNewUrlParser: true, // removes a deprecation warning when connecting
+      useUnifiedTopology: true, // removes a deprecating warning when connecting
+      //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
+      //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
+    },
+  },
+
+  migrationsDir: 'migrations-backdev',
+
+  changelogCollectionName: 'changelog',
+
+  migrationFileExtension: '.js',
+
+  // Enable the algorithm to create a checksum of the file contents and use that in the comparison to determin
+  // if the file should be run.  Requires that scripts are coded to be run multiple times.
+  useFileHash: false,
+};
+
+// Return the config as a promise
+module.exports = config;
