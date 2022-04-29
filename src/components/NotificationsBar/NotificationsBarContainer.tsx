f6156a7eb7cbab08fab6659d622d35212909034d
zhuk <evgeniy.zhuk.by@gmail.com>
2022-04-29 13:12:11 +0300

    V1-202 - fixed imports

--- src/components/NotificationsBar/NotificationsBarContainer.tsx
+++ src/components/NotificationsBar/NotificationsBarContainer.tsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 
 import { Notification as NotificationType } from 'types/notification';
 import { NOTIFICATION_STATUSES } from 'constants/statuses';
+import useReadNotifications from 'api/userInfo/setNotificationsStatus';
 
 import NotificationsBar from './NotificationsBar';
-import useReadNotifications from '../../api/userInfo/setNotificationsStatus';
 
 interface Props {
   isMobileMenuOpen: boolean;
