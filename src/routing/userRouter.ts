29b5bc5926e30e88de71ff08ce6330e577d9ea12
zhuk <evgeniy.zhuk.by@gmail.com>
2022-04-29 13:06:42 +0300

    V1-202 - frontend part of notifications

--- src/routing/userRouter.ts
+++ src/routing/userRouter.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { USER_ROLES } from 'config/constants';
 import withAuth from 'middlewares/authMiddleware';
 import getProfileInformation from 'controllers/user/getProfileInfo';
+import readNotifications from 'controllers/user/readNotifications';
 import { SubRoutes } from 'enums/routesEnum';
 
 const userRouter = Router();
@@ -11,6 +12,12 @@ userRouter.get(
   SubRoutes.getUserInfo,
   withAuth([USER_ROLES.EMPLOYEE, USER_ROLES.ADMIN, USER_ROLES.MANAGER]),
   getProfileInformation,
+);
+
+userRouter.put(
+  SubRoutes.readNotifications,
+  withAuth([USER_ROLES.EMPLOYEE, USER_ROLES.ADMIN, USER_ROLES.MANAGER]),
+  readNotifications,
 );
 
 export default userRouter;
