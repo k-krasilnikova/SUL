d95d62d6e9e0489b84ca2ba9f60604ea3d8dd87d
dapodev <apolonik.danila2002@gmail.com>
2022-05-05 14:52:25 +0300

    refactor: fixed target request loader

--- src/pages/PendingAssessments/PendingAssessmentsContainer.tsx
+++ src/pages/PendingAssessments/PendingAssessmentsContainer.tsx
@@ -1,19 +1,34 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import { useGetPendingAssessments, useManageAssessment } from 'api/manager';
+import { AssessmentManagmentAction } from 'enums/api';
 
 import PendingAssessments from './PendingAssessments';
 
 const PendingAssessmentsContainer: FC = () => {
+  const [targetId, setTargetId] = useState<string>('');
+
   const { data: assessmentsRequest, isLoading: isAssessmentsLoading } = useGetPendingAssessments();
   const { mutate: manageAssessment, isLoading: isManageAssessmentLoading } = useManageAssessment();
 
+  const approveAssessmentById = (requestId: string) => {
+    setTargetId(requestId);
+    manageAssessment({ id: requestId, action: AssessmentManagmentAction.approve });
+  };
+
+  const declineAssessmentById = (requestId: string) => {
+    setTargetId(requestId);
+    manageAssessment({ id: requestId, action: AssessmentManagmentAction.decline });
+  };
+
   return (
     <PendingAssessments
+      targetId={targetId}
       assessments={assessmentsRequest}
       isLoading={isAssessmentsLoading}
       isActionLoading={isManageAssessmentLoading}
-      manageAssessment={manageAssessment}
+      approveAssessmentById={approveAssessmentById}
+      declineAssessmentById={declineAssessmentById}
     />
   );
 };
