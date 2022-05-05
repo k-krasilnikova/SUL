3e38a8a42d2c1367c955e25647bca8eb15151225
PechurinaLena <elena-pechurina@itechart-group.com>
2022-05-05 14:04:28 +0300

    Changed LessonsStep

--- src/pages/CourseEditor/LessonsStep/LessonsStepContainer.tsx
+++ src/pages/CourseEditor/LessonsStep/LessonsStepContainer.tsx
@@ -4,54 +4,29 @@ import { useNavigate } from 'react-router';
 import LessonsStep from './LessonsStep';
 
 interface ILessonsStepContainerProps {
-  formValues?: any;
-  handleSubmit?: () => void;
+  formik: unknown;
 }
 
-const LessonsStepContainer: FC<ILessonsStepContainerProps> = ({ formValues, handleSubmit }) => {
+const LessonsStepContainer: FC<ILessonsStepContainerProps> = ({ formik }) => {
   const [selectOption, setSelectOption] = useState('video');
-  const [lessonInputValues, setLessonInputValues] = useState({
-    videoURL: formValues?.videoURL,
-    textDescription: formValues?.textDescription,
-    exerciseTitle: formValues?.exerciseTitle,
-    exerciseDescription: formValues?.exerciseDescription,
-  });
-
-  // eslint-disable-next-line no-console
-  console.log(lessonInputValues, 'lessonInputValues');
-  // eslint-disable-next-line no-console
-  console.log(formValues, 'formValues');
 
   const navigate = useNavigate();
-
-  const handleChange = (event: BaseSyntheticEvent) => {
-    setSelectOption(event.target.value);
-  };
 
   const handleBackButtonClick = (event: BaseSyntheticEvent) => {
     event.preventDefault();
     navigate(-1);
   };
 
-  const checkPastedValue = (event: BaseSyntheticEvent) => {
-    event.preventDefault();
-    const inputValue = event.target.value;
-    const formattedValue = inputValue.split(/\s+/).join(' ').trimStart().trimEnd();
-    setLessonInputValues((prev) => ({
-      ...prev,
-      [event.target.id]: formattedValue,
-    }));
+  const handleChangeOption = (event: BaseSyntheticEvent) => {
+    setSelectOption(event.target.value);
   };
 
   return (
     <LessonsStep
-      formValues={formValues}
+      formik={formik}
       handleBackButtonClick={handleBackButtonClick}
-      handleSubmit={handleSubmit}
-      handleChange={handleChange}
+      handleChangeOption={handleChangeOption}
       selectOption={selectOption}
-      checkPastedValue={checkPastedValue}
-      lessonInputValues={lessonInputValues}
     />
   );
 };
