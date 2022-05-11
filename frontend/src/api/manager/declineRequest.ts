3918a3c821a22ec37d62106d72d8723b3a5d37ef
Ostapenlo Andrei <ostapdans99@gmail.com>
2022-05-11 15:59:03 +0400

    refactor code part 3

--- frontend/src/api/manager/declineRequest.ts
+++ frontend/src/api/manager/declineRequest.ts
@@ -1,20 +1,25 @@
-import { useMutation, UseMutationResult, useQueryClient } from 'react-query';
+import { useMutation, UseMutationResult } from 'react-query';
 import { AxiosError } from 'axios';
 import { useSnackbar } from 'notistack';
 
-import { apiClientWrapper } from 'api/base';
+import { apiClientWrapper, queryClient } from 'api/base';
 import { API } from 'constants/routes';
 import { errorSnackbar } from 'constants/snackbarVariant';
 import { QUERY_KEYS } from 'constants/queryKeyConstants';
 
 const useDeclineRequest = (): UseMutationResult => {
   const { enqueueSnackbar } = useSnackbar();
-  const queryClient = useQueryClient();
+
   const handleSubmitError = (error: AxiosError) => {
     enqueueSnackbar(error?.response?.data, errorSnackbar);
   };
+  const handleSubmitSuccess = () => {
+    queryClient.invalidateQueries([QUERY_KEYS.coursesRequests]);
+    queryClient.invalidateQueries([QUERY_KEYS.profile]);
+  };
+
   return useMutation(
-    async (id: string | undefined | unknown) => {
+    async (id?: string | unknown) => {
       const data = { id };
       const apiClient = apiClientWrapper();
       const response = await apiClient.put(API.declineRequest, data);
@@ -22,9 +27,7 @@ const useDeclineRequest = (): UseMutationResult => {
     },
     {
       onError: handleSubmitError,
-      onSuccess: () => {
-        queryClient.invalidateQueries([QUERY_KEYS.coursesRequests]);
-      },
+      onSuccess: handleSubmitSuccess,
     },
   );
 };
