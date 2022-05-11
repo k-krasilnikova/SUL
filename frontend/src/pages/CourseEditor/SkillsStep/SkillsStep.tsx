df1e58f779fa60aeda713d436a8b5dc434a1abec
Irene Sinitsyna <hauteballe@gmail.com>
2022-05-11 11:37:26 +0300

    WIP

--- frontend/src/pages/CourseEditor/SkillsStep/SkillsStep.tsx
+++ frontend/src/pages/CourseEditor/SkillsStep/SkillsStep.tsx
@@ -1,5 +1,93 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { FC } from 'react';
+import { Autocomplete, Box, UseAutocompleteProps } from '@mui/material';
 
-const SkillsStep: FC = () => <div>Skills</div>;
+import { ButtonLabels } from 'constants/ButtonLabels';
+import { ICourse } from 'types/course';
+
+import { BackButton, FormWrapper, InnerWrapper, SectionName } from '../DefinitionStep/styled';
+import { SkillButton, SkillField, SkillsText, SkillWrapper } from './styled';
+import { IFormik } from '../types';
+
+export interface ISkillsStepProps {
+  ungroupedSkills: {
+    [key: string]: { group: string; image: string; maxScore: number; name: string; _id: string };
+  };
+  namesArr: string[];
+  courseData?: ICourse;
+  formik: IFormik;
+  isNextSkillVisible?: boolean;
+  handleAddSkill?: () => void;
+  handleRemoveSkill?: () => void;
+  arrOfObj1: { group: string; image: string; maxScore: number; name: string; _id: string }[];
+}
+
+const SkillsStep: FC<ISkillsStepProps> = ({
+  ungroupedSkills,
+  courseData,
+  arrOfObj1,
+  formik,
+  namesArr,
+}) => {
+  return (
+    <InnerWrapper>
+      <BackButton variant="medium" color="primary">
+        {ButtonLabels.back}
+      </BackButton>
+      <FormWrapper>
+        <SectionName>Edit course skills</SectionName>
+        <Box sx={{ marginTop: '50px', marginBottom: '25px' }}>
+          <SkillsText>Achieved skill</SkillsText>
+        </Box>
+        {courseData &&
+          courseData.technologies.map((technology: { skill: string; points: number }, index) => {
+            console.log('technology', technology);
+            const commonTechnologies = arrOfObj1.filter((commonTechnology) =>
+              commonTechnology._id === technology.skill ? commonTechnology.name : null,
+            );
+
+            console.log({ commonTechnologies });
+
+            return commonTechnologies.map((commonTechnology) => {
+              let pointsArr = [];
+              for (let i = 1; i <= commonTechnology.maxScore; i += 1) {
+                pointsArr.push(i);
+              }
+              pointsArr = pointsArr.map((point) => `${point}/${commonTechnology.maxScore}`);
+              return (
+                <>
+                  <SkillWrapper key={commonTechnology._id}>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        marginBottom: '28px',
+                      }}
+                    >
+                      <Autocomplete
+                        value={commonTechnology.name}
+                        inputValue={commonTechnology.name}
+                        options={namesArr}
+                        id="skill"
+                        renderInput={(params) => <SkillField {...params} label="Technology" />}
+                      />
+                      <Autocomplete
+                        value={`${technology.points}`}
+                        inputValue={`${technology.points}/${commonTechnology.maxScore}`}
+                        options={pointsArr}
+                        id="points"
+                        renderInput={(params) => <SkillField {...params} label="Level" />}
+                      />
+                    </Box>
+                    <SkillButton variant="mediumOutlined">{ButtonLabels.removeSkill}</SkillButton>
+                  </SkillWrapper>
+                </>
+              );
+            });
+          })}
+      </FormWrapper>
+    </InnerWrapper>
+  );
+};
 
 export default SkillsStep;
--- frontend/src/pages/CourseEditor/types.ts
+++ frontend/src/pages/CourseEditor/types.ts
@@ -1,10 +1,36 @@
-import { ICourse } from 'types/course';
+import { ChangeEventHandler } from 'react';
+import { TCourseTechnology, ICourse } from 'types/course';
 
+export interface IFormik {
+  initialValues: {
+    title?: string;
+    complexity?: number;
+    avatar?: string;
+    description?: string;
+    technologies?: TCourseTechnology[];
+  };
+  values: {
+    title?: string;
+    complexity?: number;
+    avatar?: string;
+    description?: string;
+    technologies?: TCourseTechnology[];
+  };
+  handleChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
+}
+
+export interface ICourseEditorProps {
+  basePath: string;
+  courseData?: ICourse;
+  isCourseDataLoading?: boolean;
+  formik: IFormik;
+}
 export interface IDefinitionStepContainerProps {
   courseData?: ICourse;
+  formik: IFormik;
 }
 
 export interface IDefinitionStepProps extends IDefinitionStepContainerProps {
-  handleChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  //   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   courseComplexity?: number | string;
 }
