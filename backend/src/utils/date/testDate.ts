f0d9f5e633c09d5c58545ec68fbbebf52bd37364
dapodev <apolonik.danila2002@gmail.com>
2022-05-12 10:16:54 +0300

    fix: fixed review comments

--- backend/src/utils/date/testDate.ts
+++ backend/src/utils/date/testDate.ts
@@ -3,11 +3,6 @@ import moment from 'moment';
 import { DISABLE_TEST_DAYS } from 'config/constants';
 import { UPDATE_TEST_STATUSES_JOB_START_HOURS } from 'utils/schedule/constants';
 
-/**
- * Returns an array that contains start test date and estimated finishTestDate
- * @param {number} testTimeSeconds
- * @returns {[Date, Date]} An array of 2 elements: startTestDate and finishTestDate
- */
 const generateStartAndFinishTestDates = (testTimeSeconds: number): [Date, Date] => {
   const startTestDate = new Date();
   const finishTestDate = moment(startTestDate).add(testTimeSeconds, 's').toDate();
--- backend/src/utils/schedule/constants.ts
+++ backend/src/utils/schedule/constants.ts
@@ -7,7 +7,7 @@ export const SCHEDULER_OPTIONS: Record<string, ISchedulerTimeOptions> = {
     hour: String(UPDATE_TEST_STATUSES_JOB_START_HOURS),
     minute: '0',
     second: '0',
-  } as const,
-} as const;
+  },
+};
 
 export const UPDATE_TEST_STATUSES_TIME_RANGE = 12;
