d588d508c88ab8be54420c2cc5f3729970cbb411
Irene Sinitsyna <hauteballe@gmail.com>
2022-05-12 13:45:57 +0300

    fix: rebased on develop and resolved merge conflicts

--- src/pages/DetailedCourse/DetailedCourseActions/DetailedCourseActions.tsx
+++ src/pages/DetailedCourse/DetailedCourseActions/DetailedCourseActions.tsx
@@ -9,9 +9,9 @@ import { ButtonLabels } from 'constants/ButtonLabels';
 import { COURSE_DISABLE_DAYS } from 'constants/time';
 import { PAGES } from 'constants/pages';
 import { COURSE_LABELS } from 'constants/statuses';
+import { Info } from 'enums/info';
 import { IDetailedCourseActions } from 'types/detailedCourse';
 import { convertDurationToString } from 'utils/helpers/convertDurationToString';
-import { Info } from 'enums/info';
 
 import DeleteCourseButton from './DeleteCourseButton';
 import { CourseInfoBox, DetailedCourseActionsBox } from './styled';
