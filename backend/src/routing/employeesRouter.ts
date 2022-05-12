40b421978908ad2fb1d6da08e78822db4de04919
zhuk <evgeniy.zhuk.by@gmail.com>
2022-05-12 14:03:38 +0300

    V1-202 - fixed issues with notifications

--- backend/src/routing/employeesRouter.ts
+++ backend/src/routing/employeesRouter.ts
@@ -3,27 +3,14 @@ import { Router } from 'express';
 import { USER_ROLES } from 'config/constants';
 import withAuth from 'middlewares/authMiddleware';
 import getEmployees from 'controllers/manager/getEmployees';
-import adapterManager from 'controllers/manager/adapterManager';
-import applyCourse from 'controllers/clientCourses/applyCourse';
-import updatePendingCourse from 'controllers/pendingCourses/updatePendingCourse';
 import adapterSender from 'controllers/pendingCourses/adapterSender';
 import getEmployeeInfo from 'controllers/manager/getEmployeeInfo';
 import { SubRoutes } from 'enums/routesEnum';
 import assignEmployeeCourses from 'controllers/manager/assignEmployeeCourses';
 import getEmployeeAvailableCourses from 'controllers/manager/getEmployeeAvailableCourses';
-import addNotification from 'controllers/notifications/addNotification';
 
 const employeesRouter = Router();
 
-employeesRouter.post(
-  SubRoutes.assignEmployeeCourse,
-  withAuth([USER_ROLES.MANAGER]),
-  adapterManager,
-  applyCourse,
-  updatePendingCourse,
-  addNotification,
-  adapterSender,
-);
 employeesRouter.post(
   SubRoutes.assignEmployeeCourses,
   withAuth([USER_ROLES.MANAGER]),
