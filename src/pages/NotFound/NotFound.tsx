3eb316212df6206a5c7c4774be1b0be867e01089
Ostapenlo Andrei <ostapdans99@gmail.com>
2022-05-13 15:31:28 +0400

    refactor after conversations

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,18 +1,22 @@
 import { Grid } from '@mui/material';
 import React from 'react';
 
+import PageTitle from 'components/PageTitle';
+
 import { ErrorText, NotFoundWrapper, StyledText, UnderErrorText } from './styled';
 
 const NotFound: React.FC = () => (
-  <NotFoundWrapper container>
-    <Grid item>
-      <ErrorText align="center">
-        <StyledText>404 </StyledText>
-        Error
-      </ErrorText>
-      <UnderErrorText>Page not found</UnderErrorText>
-    </Grid>
-  </NotFoundWrapper>
+  <PageTitle title="Not found">
+    <NotFoundWrapper container>
+      <Grid item>
+        <ErrorText align="center">
+          <StyledText>404 </StyledText>
+          Error
+        </ErrorText>
+        <UnderErrorText>Page not found</UnderErrorText>
+      </Grid>
+    </NotFoundWrapper>
+  </PageTitle>
 );
 
 export default NotFound;
