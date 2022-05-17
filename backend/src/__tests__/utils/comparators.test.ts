638c4207b72d97fa3a63301baadfd92a35e95eb9
zhuk <evgeniy.zhuk.by@gmail.com>
2022-05-17 11:21:09 +0300

    V1-221 added some tests for utils

--- backend/src/__tests__/utils/comparators.test.ts
+++ backend/src/__tests__/utils/comparators.test.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
+
 import { containsObjectId } from 'utils/comparator/ObjectId/containsObjectId';
+import { isEqualObjectId } from 'utils/comparator/ObjectId/compareObjectIds';
 
 describe('comparators tests', () => {
   const createObjectId = () => new mongoose.Types.ObjectId();
@@ -10,5 +12,12 @@ describe('comparators tests', () => {
     const arrayOfIds = [createObjectId(), correctId];
     expect(containsObjectId(arrayOfIds, correctId)).toBeTruthy();
     expect(containsObjectId(arrayOfIds, falsyId)).toBeFalsy();
+  });
+
+  it('should compare two ObjectIds', () => {
+    const id = createObjectId();
+
+    const isEqualObjectIds = isEqualObjectId(id, id);
+    expect(isEqualObjectIds).toBeTruthy();
   });
 });
