555bf43ba6f3adbcc9d22e555c7af6944e8229ab
dapodev <apolonik.danila2002@gmail.com>
2022-05-18 11:26:44 +0300

    refactor: refresh controller types refactored

---
+++ backend/src/interfaces/requests/auth/refresh.d.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+
+import { ITokens } from 'interfaces/Iauth/authInterfaces';
+
+type TRefreshResponsePaylaod = ITokens;
+
+type TRefreshRequest = Request;
+
+type TRefreshResponse = Response<TRefreshResponsePaylaod>;
+
+export { TRefreshRequest, TRefreshResponse };
