864dd991a42d22f7aec63d957a449aea84131b2d
dapodev <apolonik.danila2002@gmail.com>
2022-05-18 16:21:19 +0300

    refactor: getPendingAssessments controller types refactored

---
+++ src/interfaces/requests/manager/getPendingAssessments.d.ts
@@ -0,0 +1,12 @@
+import { TAssessmentRequest } from 'interfaces/IResponse/IResponse';
+
+import { TBaseRequest, TBaseResponse } from '../base';
+import { IAuthLocals } from '../common/locals';
+
+type TResponsePayload = TAssessmentRequest[];
+type TResponseLocals = IAuthLocals & { results: TResponsePayload }; // remove results in "V1-247 Remove adapters"
+
+type TGetPendingAssessmentsRequest = TBaseRequest;
+type TGetPendingAssessmentsResponse = TBaseResponse<TResponsePayload, TResponseLocals>;
+
+export { TGetPendingAssessmentsRequest, TGetPendingAssessmentsResponse };
