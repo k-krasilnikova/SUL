f1d3596bd4ee4260de04e7ea2d00f1b28ba2f246
dapodev <apolonik.danila2002@gmail.com>
2022-05-18 13:56:05 +0300

    refactor: getMaterials controller types refactored

---
+++ src/interfaces/requests/courses/getMaterials.d.ts
@@ -0,0 +1,12 @@
+import { ICourse } from 'interfaces/Ientities/Icourses';
+import { TBaseRequest, TBaseResponse } from '../base';
+import { IParamsId } from '../common/params';
+
+type TGetMaterialsRequestParams = IParamsId;
+
+type TGetMaterialsResponsePayload = Pick<ICourse, '_id' | 'materials'>; // actually should return only IMaterial[]
+
+type TGetMaterialsRequest = TBaseRequest<TGetMaterialsRequestParams>;
+type TGetMaterialsResponse = TBaseResponse<TGetMaterialsResponsePayload>;
+
+export { TGetMaterialsRequest, TGetMaterialsResponse };
