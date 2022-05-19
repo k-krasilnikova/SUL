69dab7873fee0469c601377fa0b5bf48a0f3bf2f
k-krasilnikova <k.krasilnikova@itechart-group.com>
2022-05-19 12:02:53 +0300

    Fixed warning

--- src/components/Layout/AuthorizedLayout/Menu/Badge/utils.ts
+++ src/components/Layout/AuthorizedLayout/Menu/Badge/utils.ts
@@ -1,7 +1,7 @@
 import { BadgeType } from 'enums/badgeType';
 import { getUserProfileCache } from 'utils/cache';
 
-export const getBadgeContentByType = (type?: BadgeType) => {
+export const getBadgeContentByType = (type?: BadgeType): number | undefined => {
   const userProfileCache = getUserProfileCache();
 
   const USER_FIELDS_BY_TYPE = {
