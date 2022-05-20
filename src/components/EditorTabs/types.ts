4debe626af0b75a62886297f16466291c3ae9f12
PechurinaLena <elena-pechurina@itechart-group.com>
2022-05-20 14:06:31 +0300

    Changed EditorTabs

--- src/components/EditorTabs/types.ts
+++ src/components/EditorTabs/types.ts
@@ -4,9 +4,15 @@ export interface IEditorTabsProps {
   currentChild?: ReactNode;
   handleNextStep?: () => void;
   handlePreviousStep?: () => void;
+  handleSubmit?: () => void;
+  isFirstStep?: boolean;
+  isLastStep?: boolean;
 }
 
 export interface IActionButtons {
   handlePreviousStep?: () => void;
   handleNextStep?: () => void;
+  handleSubmit?: () => void;
+  isFirstStep?: boolean;
+  isLastStep?: boolean;
 }
