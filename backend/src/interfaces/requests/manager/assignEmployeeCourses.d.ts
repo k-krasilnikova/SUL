b59019a0a6cbeafaeebf9c625a6cc5f94cb48881
dapodev <apolonik.danila2002@gmail.com>
2022-05-24 11:42:28 +0300

    refactor: removed results fields from types

--- backend/src/interfaces/requests/manager/assignEmployeeCourses.d.ts
+++ backend/src/interfaces/requests/manager/assignEmployeeCourses.d.ts
@@ -8,9 +8,7 @@ type TRequestParams = IIdParams;
 type TRequestBody = ICourseToAssign[];
 
 type TResponsePayload = string;
-type TResponseLocals = IAuthLocals & {
-  results: TResponsePayload; // remove results in "V1-247 Remove adapters"
-};
+type TResponseLocals = IAuthLocals;
 
 type TAssignEmployeeCoursesRequest = TBaseRequest<TRequestParams, TRequestBody>;
 type TAssignEmployeeCoursesResponse = TBaseResponse<TResponsePayload, TResponseLocals>;
