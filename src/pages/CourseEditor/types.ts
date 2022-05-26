1b9947dbe23b5687b8fe388904f2b2599e3b71ec
PechurinaLena <elena-pechurina@itechart-group.com>
2022-05-26 12:50:03 +0300

    Fixed CourseEditor after conversation

--- src/pages/CourseEditor/types.ts
+++ src/pages/CourseEditor/types.ts
@@ -77,3 +77,9 @@ export interface ICourseEditorMaterials {
   material: string;
   exercise?: { eN: number; title: string; task: string; code: string };
 }
+
+export interface ICourseEditorMaterials {
+  type: string;
+  material: string;
+  exercise?: { eN: number; title: string; task: string; code: string };
+}
