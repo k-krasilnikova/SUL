b5680b508332e7087b3cacd73d986e8a3ccfb17e
dapodev <apolonik.danila2002@gmail.com>
2022-05-27 12:50:28 +0300

    test: added tests for login mappers

---
+++ backend/src/__tests__/utils/mappers/login.test.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+import { mapLoginPayload } from 'controllers/auth/login/utils/mappers';
+import { ITokens } from 'interfaces/Iauth/authInterfaces';
+
+describe('Login mappers tests', () => {
+  const createObjectId = () => new mongoose.Types.ObjectId();
+
+  it('Map login payload', () => {
+    const userId = createObjectId();
+    const tokens: ITokens = { accessToken: 'accessTokenMock', refreshToken: 'refreshTokenMock' };
+
+    const payload = mapLoginPayload(tokens, userId);
+
+    expect(payload).toEqual({
+      accessToken: tokens.accessToken,
+      refreshToken: tokens.refreshToken,
+      _id: userId,
+    });
+  });
+});
