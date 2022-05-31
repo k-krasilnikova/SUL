dcff9dc2aad4df4660f16b2348b1350dc1702406
Evgeny <evgeniy.zhuk.by@gmail.com>
2022-05-31 14:10:55 +0300

    V1-247 moved types to payloads.d.ts

--- src/interfaces/requests/notifications/addNotification.d.ts
+++ src/interfaces/requests/notifications/addNotification.d.ts
@@ -1,11 +1,13 @@
 import { TBaseRequest, TBaseResponse } from '../base';
 import { IAuthLocals } from '../common/locals';
+import { IAddNotificationPayload } from '../common/payloads';
 
 type TResponsePayload = never;
+type TResponseLocals = IAuthLocals;
 
-type TRequestBody = { courseId: string; clientCourseId: string; assessment: boolean };
+type TRequestBody = IAddNotificationPayload;
 
-type TAddNotificationRequest = TBaseRequest<IAuthLocals, TRequestBody>;
-type TAddNotificationResponse = TBaseResponse<TResponsePayload>;
+type TAddNotificationRequest = TBaseRequest<TResponseLocals, TRequestBody>;
+type TAddNotificationResponse = TBaseResponse<TResponsePayload, TResponseLocals>;
 
 export { TAddNotificationRequest, TAddNotificationResponse };
