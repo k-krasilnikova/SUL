6f0571fbf7518ba4732b3dd543bace82394520b1
PechurinaLena <elena-pechurina@itechart-group.com>
2022-06-01 12:16:05 +0300

    Changed SkillItem

--- frontend/src/components/SkillGroup/SkillItem/SkillItem.tsx
+++ frontend/src/components/SkillGroup/SkillItem/SkillItem.tsx
@@ -1,41 +1,23 @@
 import { FC } from 'react';
 
 import Avatar from 'components/Avatar';
+import Tooltip from 'components/Tooltip';
 import { Size } from 'enums/sizes';
-import { Length, Numbers } from 'enums/numbers';
 
-import {
-  SkillContainer,
-  ImageWrapper,
-  SkillNameWrapper,
-  SkillName,
-  HoverSkillName,
-} from './styled';
+import { SkillContainer, ImageWrapper, SkillNameWrapper, SkillName } from './styled';
 import { ISkillItemProps } from '../types';
 
-const SkillItem: FC<ISkillItemProps> = ({
-  name,
-  skillImage,
-  isSkillNameShown,
-  showSkillName,
-  hideSkillName,
-}) => {
-  const shortSkillName =
-    name.length > Length.maxSkillLength
-      ? `${name.slice(Numbers.zero, Length.maxSkillLength)}...`
-      : name;
-
-  return (
-    <SkillContainer>
-      <ImageWrapper>
-        <Avatar size={Size.submedium} avatar={skillImage} />
-      </ImageWrapper>
-      <SkillNameWrapper onMouseEnter={showSkillName} onMouseLeave={hideSkillName}>
-        <SkillName>{shortSkillName}</SkillName>
-        {isSkillNameShown && <HoverSkillName>{name}</HoverSkillName>}
-      </SkillNameWrapper>
-    </SkillContainer>
-  );
-};
+const SkillItem: FC<ISkillItemProps> = ({ name, skillImage }) => (
+  <SkillContainer>
+    <ImageWrapper>
+      <Avatar size={Size.submedium} avatar={skillImage} />
+    </ImageWrapper>
+    <SkillNameWrapper>
+      <Tooltip title={name}>
+        <SkillName>{name}</SkillName>
+      </Tooltip>
+    </SkillNameWrapper>
+  </SkillContainer>
+);
 
 export default SkillItem;
