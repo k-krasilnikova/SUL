7f2465421bd408daba61c4ccb56b7ebed16c9265
PechurinaLena <44974457+PechurinaLena@users.noreply.github.com>
2022-06-06 17:09:22 +0300

    Fixed SkillName component

--- frontend/src/components/SkillGroup/SkillItem/SkillItem.tsx
+++ frontend/src/components/SkillGroup/SkillItem/SkillItem.tsx
@@ -3,21 +3,28 @@ import { FC } from 'react';
 import Avatar from 'components/Avatar';
 import Tooltip from 'components/Tooltip';
 import { Size } from 'enums/sizes';
+import { Numbers } from 'enums/numbers';
+import { ISkillItemProps } from 'components/SkillGroup/types';
 
 import { SkillContainer, ImageWrapper, SkillNameWrapper, SkillName } from './styled';
-import { ISkillItemProps } from '../types';
 
-const SkillItem: FC<ISkillItemProps> = ({ name, skillImage }) => (
-  <SkillContainer>
-    <ImageWrapper>
-      <Avatar size={Size.submedium} avatar={skillImage} />
-    </ImageWrapper>
-    <SkillNameWrapper>
-      <Tooltip title={name}>
-        <SkillName>{name}</SkillName>
-      </Tooltip>
-    </SkillNameWrapper>
-  </SkillContainer>
-);
+const SkillItem: FC<ISkillItemProps> = ({ name, skillImage }) => {
+  const NAME_MAX_LENGTH = 30;
+  const shortSkillName =
+    name.length > NAME_MAX_LENGTH ? `${name.slice(Numbers.zero, NAME_MAX_LENGTH)}...` : name;
+
+  return (
+    <SkillContainer>
+      <ImageWrapper>
+        <Avatar size={Size.submedium} avatar={skillImage} />
+      </ImageWrapper>
+      <SkillNameWrapper>
+        <Tooltip title={shortSkillName}>
+          <SkillName>{shortSkillName}</SkillName>
+        </Tooltip>
+      </SkillNameWrapper>
+    </SkillContainer>
+  );
+};
 
 export default SkillItem;
