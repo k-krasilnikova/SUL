eb286d4c9861f43de4f3a152d75bd0517b846ee9
PechurinaLena <44974457+PechurinaLena@users.noreply.github.com>
2022-06-07 11:55:27 +0300

    Fixed SkillItem after conversation

--- src/pages/Skills/SkillGroup/SkillItem/SkillItem.tsx
+++ src/pages/Skills/SkillGroup/SkillItem/SkillItem.tsx
@@ -3,28 +3,22 @@ import { FC } from 'react';
 import Avatar from 'components/Avatar';
 import Tooltip from 'components/Tooltip';
 import { Size } from 'enums/sizes';
-import { Numbers } from 'enums/numbers';
 import { ISkillItemProps } from 'pages/Skills/types';
+import { getShortedSkillName } from 'pages/Skills/utils';
 
 import { SkillContainer, ImageWrapper, SkillNameWrapper, SkillName } from './styled';
 
-const SkillItem: FC<ISkillItemProps> = ({ name, skillImage }) => {
-  const NAME_MAX_LENGTH = 25;
-  const shortSkillName =
-    name.length > NAME_MAX_LENGTH ? `${name.slice(Numbers.zero, NAME_MAX_LENGTH)}...` : name;
-
-  return (
-    <SkillContainer>
-      <ImageWrapper>
-        <Avatar size={Size.submedium} avatar={skillImage} />
-      </ImageWrapper>
-      <SkillNameWrapper>
-        <Tooltip title={shortSkillName}>
-          <SkillName>{shortSkillName}</SkillName>
-        </Tooltip>
-      </SkillNameWrapper>
-    </SkillContainer>
-  );
-};
+const SkillItem: FC<ISkillItemProps> = ({ name, skillImage }) => (
+  <SkillContainer>
+    <ImageWrapper>
+      <Avatar size={Size.submedium} avatar={skillImage} />
+    </ImageWrapper>
+    <SkillNameWrapper>
+      <Tooltip title={name}>
+        <SkillName>{getShortedSkillName(name)}</SkillName>
+      </Tooltip>
+    </SkillNameWrapper>
+  </SkillContainer>
+);
 
 export default SkillItem;
