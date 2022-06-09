a8a885561e42345e7e7531a913f2d07dcceb472d
Ostapenlo Andrei <ostapdans99@gmail.com>
2022-06-09 21:09:21 +0400

    Fix | V1-263 - Fixed Action Buttons at Pending Request page

---
+++ src/pages/Requests/types.ts
@@ -0,0 +1,26 @@
+import { IApproveCourseDto } from 'types/api.dto';
+import { IRequest } from 'types/request';
+
+
+
+export interface IRequestsProps {
+  actionTarget: IApproveCourseDto;
+  isRequestsLoading: boolean;
+  isApproveLoading: boolean;
+  isDeclineLoading: boolean;
+  approveRequest: (params: IApproveCourseDto) => void;
+  declineRequest: (requestId: string) => void;
+  requests?: IRequest[];
+}
+
+export interface IRequestItemProps
+  extends Pick<
+    IRequestsProps,
+    'actionTarget' | 'isApproveLoading' | 'isDeclineLoading' | 'approveRequest' | 'declineRequest'
+  > {
+  request: IRequest;
+}
+
+export interface IRequestButtonsProps extends Omit<IRequestItemProps, 'request'> {
+  requestId: string;
+}
