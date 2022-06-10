7e5786fd9655d6a9e37473061cc7d5c22416e237
Aliaksandr <aliaksandr@wse-050-71b.icx.local>
2022-06-10 19:40:02 +0300

    resolve conversation

--- src/pages/Skills/SkillsContainer.tsx
+++ src/pages/Skills/SkillsContainer.tsx
@@ -1,20 +1,15 @@
-import { FC, useState, useEffect, BaseSyntheticEvent } from 'react';
+import { FC, useState, BaseSyntheticEvent } from 'react';
 
 import { useDebounce } from 'hooks';
 import { useGetSkills } from 'api/skills';
 import { formatInputValue } from 'utils/helpers/searchHelpers';
-import { ISkillsListProps } from 'types/skill';
 
 import Skills from './Skills';
 
 const SkillsContainer: FC = () => {
   const [searchInputValue, setSearchInputValue] = useState<string>('');
-  const [skillFounded, setSkillFounded] = useState<ISkillsListProps[] | undefined>([]);
   const debouncedSearchValue = useDebounce(searchInputValue);
   const { data: skillsResponse, isLoading: isLoadingSkills } = useGetSkills(debouncedSearchValue);
-  useEffect(() => {
-    setSkillFounded(skillsResponse);
-  }, [skillsResponse]);
 
   const handleSearchInputChange = ({ target }: BaseSyntheticEvent) => {
     setSearchInputValue(formatInputValue(target.value));
@@ -22,7 +17,7 @@ const SkillsContainer: FC = () => {
 
   return (
     <Skills
-      skillFounded={skillFounded}
+      skillFounded={skillsResponse}
       handleSearchInputChange={handleSearchInputChange}
       isSkillsLoading={isLoadingSkills}
     />
