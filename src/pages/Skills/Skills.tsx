6cff5879d20866217faa48f9dc06ae833ed8c5ab
PechurinaLena <44974457+PechurinaLena@users.noreply.github.com>
2022-06-15 15:21:22 +0300

    Fixed Skill components for admin

--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -3,6 +3,8 @@ import { FC } from 'react';
 import PageTitle from 'components/PageTitle';
 import Loader from 'components/Loader';
 import { Loaders } from 'enums/loader';
+import NoContent from 'components/NoContent';
+import { NO_SKILLS } from 'constants/messages';
 
 import SkillGroup from './SkillGroup';
 import { ISkillsPageProps } from './types';
@@ -12,28 +14,30 @@ import {
   SearchWrapper,
   SkillsWrapper,
   StyledDivider,
+  NoSkills,
 } from './styled';
 
-const Skills: FC<ISkillsPageProps> = ({
-  skillFounded,
-  handleSearchInputChange,
-  isSkillsLoading,
-}) => (
-  <PageTitle title="Skills">
-    <SkillsPageContainer container>
-      <SkillsWrapper>
-        <SearchWrapper>
-          <SearchSkill onChange={handleSearchInputChange} />
-          <StyledDivider />
-        </SearchWrapper>
-        {isSkillsLoading ? (
-          <Loader type={Loaders.content} />
-        ) : (
-          <SkillGroup skillFounded={skillFounded} />
-        )}
-      </SkillsWrapper>
-    </SkillsPageContainer>
-  </PageTitle>
-);
+const Skills: FC<ISkillsPageProps> = ({ skillFounded, handleSearchInputChange, isSkillsLoading }) =>
+  isSkillsLoading ? (
+    <Loader type={Loaders.content} />
+  ) : (
+    <PageTitle title="Skills">
+      <SkillsPageContainer container>
+        <SkillsWrapper>
+          <SearchWrapper>
+            <SearchSkill onChange={handleSearchInputChange} />
+            <StyledDivider />
+          </SearchWrapper>
+          {skillFounded?.length ? (
+            <SkillGroup skillFounded={skillFounded} />
+          ) : (
+            <NoSkills>
+              <NoContent message={NO_SKILLS} />
+            </NoSkills>
+          )}
+        </SkillsWrapper>
+      </SkillsPageContainer>
+    </PageTitle>
+  );
 
 export default Skills;
